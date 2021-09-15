251fa05e04c715ea88b6fda78933b84f4898e9ef
Tarrence van As <tarrence13@gmail.com>
2021-09-14 18:02:23 -0700

    connect wallet component

---
+++ packages/web/components/ConnectWallet.tsx
@@ -0,0 +1,71 @@
+import { css } from '@emotion/react';
+import { useCallback } from 'react';
+
+import ConnectWalletSVG from '../svg/ConnectWallet';
+import Dialog from '../components/Dialog';
+
+import useWeb3Provider from '../hooks/web3';
+
+const ConnectWallet = () => {
+  const { connect } = useWeb3Provider();
+
+  const buttonProps = {
+    className: 'button',
+    css: css`
+      font-size: var(--text-02);
+    `,
+  };
+
+  const onClick = useCallback(
+    async (w: 'MetaMask' | 'WalletConnect') => {
+      try {
+        await connect(w);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    [connect],
+  );
+
+  return (
+    <Dialog
+      css={css`
+        background: none;
+      `}
+    >
+      <div
+        css={css`
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          gap: 25px;
+          width: 390px;
+
+          @media (max-width: 500px) {
+            width: 275px;
+          }
+        `}
+      >
+        <ConnectWalletSVG />
+        <div>Please connect your Ethereum Wallet</div>
+        <div
+          css={css`
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            gap: 10px;
+          `}
+        >
+          <button {...buttonProps} onClick={() => onClick('MetaMask')}>
+            MetaMask
+          </button>
+          <button {...buttonProps} onClick={() => onClick('WalletConnect')}>
+            WalletConnect
+          </button>
+        </div>
+      </div>
+    </Dialog>
+  );
+};
+
+export default ConnectWallet;
