8e4909cd7c41d43ac83c016bb43e326f269b9dc4
Tarrence van As <tarrence13@gmail.com>
2021-09-14 20:13:56 -0700

    add dope table

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -1,12 +1,12 @@
 import '../styles/reset.css';
 
 import type { AppProps } from 'next/app';
-import { css } from '@emotion/css';
 
 import { NetworkIDs } from '@zoralabs/nft-hooks';
 import { MediaConfiguration } from '@zoralabs/nft-components';
 import { Web3ReactProvider } from '@web3-react/core';
 import { Web3Provider } from '@ethersproject/providers';
+import { ChakraProvider } from '@chakra-ui/react';
 
 import { mediaConfigurationStyles } from '../styles/theme';
 import GlobalStyles from '../styles/GlobalStyles';
@@ -23,18 +23,20 @@ export default function CreateDopeApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyles />
-      <Web3ReactProvider getLibrary={getLibrary}>
-        <MediaConfiguration
-          networkId={process.env.NEXT_PUBLIC_NETWORK as NetworkIDs}
-          style={mediaConfigurationStyles}
-        >
-          <Header />
-          <main>
-            <Component {...pageProps} />
-          </main>
-          {/* <Footer /> */}
-        </MediaConfiguration>
-      </Web3ReactProvider>
+      <ChakraProvider>
+        <Web3ReactProvider getLibrary={getLibrary}>
+          <MediaConfiguration
+            networkId={process.env.NEXT_PUBLIC_NETWORK as NetworkIDs}
+            style={mediaConfigurationStyles}
+          >
+            <Header />
+            <main>
+              <Component {...pageProps} />
+            </main>
+            {/* <Footer /> */}
+          </MediaConfiguration>
+        </Web3ReactProvider>
+      </ChakraProvider>
     </>
   );
 }
