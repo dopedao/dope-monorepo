a98338876459a00a009e490f3cb06b9215936dfe
faces-of-eth <facesofeth@proton.me>
2021-09-17 05:40:35 -0700

    Fix window height

--- packages/web/components/AppWindow.tsx
+++ packages/web/components/AppWindow.tsx
@@ -6,11 +6,11 @@ import { Footer } from './Footer';
 const AppWindowWrapper = styled(PageWrapper)`
   // max-width: var(--content-width-xl);
   width: 100%;
-  height: 200px;
   background-color: #ffffff;
   padding: 0;
   margin: 2vh;
   border: 2px solid #000;
+  filter: drop-shadow(8px 8px rgba(0,0,0, 0.15));
 `;
 
 export default function AppWindow({ children }) {
