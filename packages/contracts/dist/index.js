61765fb3b5d508f0560c402921f0f04cd171e184
Tarrence van As <tarrence13@gmail.com>
2021-09-17 22:39:52 -0700

    commit contract bindings

---
+++ packages/contracts/dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Stockpile__factory = exports.Paper__factory = exports.Character__factory = void 0;
+var Character__factory_1 = require("./factories/Character__factory");
+Object.defineProperty(exports, "Character__factory", { enumerable: true, get: function () { return Character__factory_1.Character__factory; } });
+var Paper__factory_1 = require("./factories/Paper__factory");
+Object.defineProperty(exports, "Paper__factory", { enumerable: true, get: function () { return Paper__factory_1.Paper__factory; } });
+var Stockpile__factory_1 = require("./factories/Stockpile__factory");
+Object.defineProperty(exports, "Stockpile__factory", { enumerable: true, get: function () { return Stockpile__factory_1.Stockpile__factory; } });
+//# sourceMappingURL=index.js.map
