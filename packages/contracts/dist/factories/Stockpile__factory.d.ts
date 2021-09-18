016506b1a263a7e7d842f2f488ecc699b8f5f407
Tarrence van As <tarrence13@gmail.com>
2021-09-17 22:39:52 -0700

    commit contract bindings

---
+++ packages/contracts/dist/factories/Stockpile__factory.d.ts
@@ -0,0 +1,66 @@
+import { Signer } from "ethers";
+import { Provider } from "@ethersproject/providers";
+import type { Stockpile, StockpileInterface } from "../Stockpile";
+export declare class Stockpile__factory {
+    static readonly abi: ({
+        inputs: {
+            internalType: string;
+            name: string;
+            type: string;
+        }[];
+        stateMutability: string;
+        type: string;
+        anonymous?: undefined;
+        name?: undefined;
+        outputs?: undefined;
+    } | {
+        anonymous: boolean;
+        inputs: {
+            indexed: boolean;
+            internalType: string;
+            name: string;
+            type: string;
+        }[];
+        name: string;
+        type: string;
+        stateMutability?: undefined;
+        outputs?: undefined;
+    } | {
+        inputs: {
+            internalType: string;
+            name: string;
+            type: string;
+        }[];
+        name: string;
+        outputs: {
+            internalType: string;
+            name: string;
+            type: string;
+        }[];
+        stateMutability: string;
+        type: string;
+        anonymous?: undefined;
+    } | {
+        inputs: {
+            internalType: string;
+            name: string;
+            type: string;
+        }[];
+        name: string;
+        outputs: {
+            components: {
+                internalType: string;
+                name: string;
+                type: string;
+            }[];
+            internalType: string;
+            name: string;
+            type: string;
+        }[];
+        stateMutability: string;
+        type: string;
+        anonymous?: undefined;
+    })[];
+    static createInterface(): StockpileInterface;
+    static connect(address: string, signerOrProvider: Signer | Provider): Stockpile;
+}
---
+++ packages/contracts/dist/index.d.ts
@@ -0,0 +1,6 @@
+export type { Character } from "./Character";
+export type { Paper } from "./Paper";
+export type { Stockpile } from "./Stockpile";
+export { Character__factory } from "./factories/Character__factory";
+export { Paper__factory } from "./factories/Paper__factory";
+export { Stockpile__factory } from "./factories/Stockpile__factory";
