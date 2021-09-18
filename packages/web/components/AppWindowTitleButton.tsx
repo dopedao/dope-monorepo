0b8473325be0cae8bf1d039b5f48928a10a799ae
Tarrence van As <tarrence13@gmail.com>
2021-09-17 21:18:15 -0700

    fix lint issues

--- packages/web/components/AppWindowTitleButton.tsx
+++ packages/web/components/AppWindowTitleButton.tsx
@@ -1,39 +1,36 @@
-import { css } from "@emotion/react";
+import { css } from '@emotion/react';
 
 interface IconProps {
-  icon: string,
-  clickAction: () => void
-};
+  icon: string;
+  clickAction: () => void;
+}
 
-export default ({ icon, clickAction }: IconProps) => {
-
+const AppWindowTitleButton = ({ icon, clickAction }: IconProps) => {
   return (
-    <div css={css`
-      background-color: #D0D0D0;
-      box-shadow: -1px -1px 0px rgba(0,0,0,.25) inset, 
-      1px 1px 0px rgba(255,255,255,.25) inset;
-      display: inline-block;
-      width: 32px;
-      height: 32px;
-      border-right: 2px solid #000;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      cursor: pointer;
-      cursor: hand;
-      img {
-        display: block;
-        width: 16px;
-        height: 16px;
-      }
-    `}
+    <div
+      css={css`
+        background-color: #d0d0d0;
+        box-shadow: -1px -1px 0px rgba(0, 0, 0, 0.25) inset,
+          1px 1px 0px rgba(255, 255, 255, 0.25) inset;
+        display: inline-block;
+        width: 32px;
+        height: 32px;
+        border-right: 2px solid #000;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        cursor: pointer;
+        cursor: hand;
+        img {
+          display: block;
+          width: 16px;
+          height: 16px;
+        }
+      `}
     >
-      <img 
-        src={ `/images/icon/${icon}.svg` } 
-        alt={ icon } 
-        onClick={ () => clickAction() } 
-      />
+      <img src={`/images/icon/${icon}.svg`} alt={icon} onClick={() => clickAction()} />
     </div>
   );
-
 };
+
+export default AppWindowTitleButton;
