9351d8233c0f0d37ccb08897b9c90d5b60aa5404
Tarrence van As <tarrencev@users.noreply.github.com>
2021-09-19 16:01:00 -0700

    pixelart rle encoder (#6)

--- packages/contracts/scripts/contract-size.sh
+++ packages/contracts/scripts/contract-size.sh
@@ -2,17 +2,10 @@
 
 set -eo pipefail
 
+# import the deployment helpers
 . $(dirname $0)/common.sh
 
-if [[ -z $contract ]]; then
-  if [[ -z ${1} ]];then
-    echo '"$contract" env variable is not set. Set it to the name of the contract you want to estimate gas cost for.'
-    exit 1
-  else
-    contract=${1}
-  fi
-fi
-contract_size=$(contract_size ${contract})
-echo "Contract Name: ${contract}"
+contract_size=$(contract_size Stockpile 0x7988fCA891B30cF8E23459B09b851F9d79B17215./scripts/cont)
+echo "Contract Name: Stockpile"
 echo "Contract Size: ${contract_size} bytes"
 echo "$(( 24576 - ${contract_size} )) bytes left to reach the smart contract size limit of 24576 bytes."
