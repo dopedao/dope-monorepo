1d6aa5a451eea868c6bd53f668d518cd8efe91e1
Tarrence van As <tarrence13@gmail.com>
2021-09-20 16:42:18 -0700

    use metamask network

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -1,15 +1,18 @@
 import '../styles/reset.css';
 
+import { ReactNode, useMemo } from 'react';
 import type { AppProps } from 'next/app';
 import { Web3ReactProvider } from '@web3-react/core';
 import { Web3Provider } from '@ethersproject/providers';
 import { ApolloProvider } from '@apollo/client';
 import { ChakraProvider } from '@chakra-ui/react';
+import { useWeb3React } from '@web3-react/core';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 import DesktopIconList from '../components/DesktopIconList';
 import theme from '../styles/theme';
 import GlobalStyles from '../styles/GlobalStyles';
-import client from '../common/apollo';
+import { NETWORK } from '../common/constants';
 
 function getLibrary(provider: any): Web3Provider {
   const library = new Web3Provider(provider);
@@ -17,20 +20,38 @@ function getLibrary(provider: any): Web3Provider {
   return library;
 }
 
+function getClient(uri: string) {
+  return new ApolloClient({
+    uri,
+    cache: new InMemoryCache(),
+  });
+}
+
+const WrappedApolloProvider = ({ children }: { children: ReactNode }) => {
+  const { chainId } = useWeb3React();
+
+  const uri = useMemo(
+    () => (chainId ? NETWORK[chainId as 1 | 4].subgraph : NETWORK[1].subgraph),
+    [chainId],
+  );
+
+  return <ApolloProvider client={getClient(uri)}>{children}</ApolloProvider>;
+};
+
 export default function CreateDopeApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyles />
-      <ApolloProvider client={client}>
-        <ChakraProvider theme={theme}>
-          <Web3ReactProvider getLibrary={getLibrary}>
+      <ChakraProvider theme={theme}>
+        <Web3ReactProvider getLibrary={getLibrary}>
+          <WrappedApolloProvider>
             <main>
               <DesktopIconList />
               <Component {...pageProps} />
             </main>
-          </Web3ReactProvider>
-        </ChakraProvider>
-      </ApolloProvider>
+          </WrappedApolloProvider>
+        </Web3ReactProvider>
+      </ChakraProvider>
     </>
   );
 }
