88023624984e97bba01652ea5fcba07fc55ea25a
Tarrence van As <tarrence13@gmail.com>
2021-09-20 17:47:54 -0700

    fix lint

--- packages/web/hooks/web3.ts
+++ packages/web/hooks/web3.ts
@@ -13,7 +13,7 @@ export const walletconnect = new WalletConnectConnector({
   qrcode: true,
 });
 
-const useWeb3Provider = () => {
+const useWeb3Provider = (): { connect: (wallet: 'MetaMask' | 'WalletConnect') => Promise<void> } => {
   const { activate } = useWeb3React();
 
   const connect = useCallback(
