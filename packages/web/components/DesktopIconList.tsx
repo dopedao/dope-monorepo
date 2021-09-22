f9cf04e7732a0a137bec6df238dfc18b2c7f0cf4
subimage <12508+subimage@users.noreply.github.com>
2021-09-22 15:21:46 -0700

    Desktop icon responsive grid (#16)
    * Ensure onClick fires for DesktopIcon on label and icon
    
    * Responsive icon grid and more contrast for icons on all backgrounds
    
    * Lint
    
    * Phone responsive fixes

--- packages/web/components/DesktopIconList.tsx
+++ packages/web/components/DesktopIconList.tsx
@@ -1,6 +1,40 @@
-import { css } from '@emotion/react';
+import { media } from '../styles/mixins';
 import { useRouter } from 'next/router';
 import DesktopIcon from '../components/DesktopIcon';
+import styled from '@emotion/styled';
+
+const IconGrid = styled.div`
+  position: fixed;
+  z-index: 0;
+  display: flex;
+  ${media.phone`
+    width: 100%;
+    height: auto;
+    flex-flow: row wrap;
+    justify-content: flex-start;
+    align-items: flex-end;
+    gap: 12px;
+    padding: 12px;
+  `}
+  ${media.tablet`
+    width: auto;
+    height: 100%;
+    flex-flow: column wrap;
+    justify-content: flex-start;
+    align-items: flex-start;
+    gap: 16px;
+    padding: 16px;
+  `}
+  ${media.laptop`
+    width: auto;
+    height: 100%;
+    flex-flow: column wrap;
+    justify-content: flex-start;
+    align-items: flex-start;
+    gap: 24px;
+    padding: 24px;
+  `}
+`;
 
 export default () => {
   const router = useRouter();
@@ -25,16 +59,18 @@ export default () => {
     openBrowserTab('https://discord.gg/6fqqBS7mhY');
   };
 
+  const openTally = (): void => {
+    openBrowserTab('https://www.withtally.com/governance/dopeWars');
+  };
+
   return (
-    <div
-      css={css`
-        position: fixed;
-      `}
-    >
+    <IconGrid>
       <DesktopIcon icon="dopewars-exe" label="DOPEWARS.EXE" clickAction={openDopewars} />
+      <DesktopIcon icon="file" label="ABOUT" clickAction={openTally} />
+      <DesktopIcon icon="tally" label="Dope DAO" clickAction={openTally} />
       <DesktopIcon icon="open-sea" label="OpenSea" clickAction={openSea} />
       <DesktopIcon icon="twitter" label="Twitter" clickAction={openTwitter} />
       <DesktopIcon icon="discord" label="Discord" clickAction={openDiscord} />
-    </div>
+    </IconGrid>
   );
 };
