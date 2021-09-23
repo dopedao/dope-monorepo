fe67506e16e9ca16c3d67eff30748a322e68d962
faces-of-eth <facesofeth@proton.me>
2021-09-22 17:25:02 -0700

    Simple responsive loot table for phones and tablets that stacks vertically (#19)

--- packages/web/styles/breakpoints.ts
+++ packages/web/styles/breakpoints.ts
@@ -25,3 +25,8 @@ export const returnBreakpoint = (breakpointName: string): string | 0 => {
   const bp = breakpoints.find(array => array.viewport === breakpointName);
   return bp === undefined ? 0 : `${bp.width}px`;
 };
+
+export const getBreakpointWidth = (breakpointName: string): number | 0 => {
+  const bp = breakpoints.find(array => array.viewport === breakpointName);
+  return bp === undefined ? 0 : bp.width;
+};
