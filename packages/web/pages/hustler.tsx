a334f60c1cb026fa58b6b8745ff078fa92abbe71
faces-of-eth <facesofeth@proton.me>
2021-09-22 19:49:25 -0700

    Coming soon dialog (#21)

--- packages/web/pages/hustler.tsx
+++ packages/web/pages/hustler.tsx
@@ -1,14 +1,15 @@
 import { useWeb3React } from '@web3-react/core';
 import AppWindow from '../components/AppWindow';
+import ComingSoon from '../components/ComingSoon';
 import Head from '../components/Head';
 
 export default function Hustler() {
   const { account } = useWeb3React();
 
   return (
-    <AppWindow requiresWalletConnection={true}>
-      <Head title="Hustler" />
-      {account ? 'hustler' : null}
+    <AppWindow padBody={false}>
+      <Head title="Create Your Hustler" />
+      <ComingSoon />
     </AppWindow>
   );
 }
