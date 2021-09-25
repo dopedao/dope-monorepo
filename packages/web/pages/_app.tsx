c0fca882b473ceca355704fd1d5532bd15375cb3
subimage <12508+subimage@users.noreply.github.com>
2021-09-24 21:45:48 -0700

    Add Sentry.io error tracking (#29)

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -15,6 +15,19 @@ import { NETWORK } from '../common/constants';
 import DesktopIconList from '../components/DesktopIconList';
 import PageLoadingIndicator from '../components/PageLoadingIndicator';
 
+// Error tracking and tracing from Sentry.io
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+Sentry.init({
+  dsn: "https://b8e63fda3aef4e8e8c96109550d5ae4c@o1012827.ingest.sentry.io/5978399",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
 function getLibrary(provider: any): Web3Provider {
   const library = new Web3Provider(provider);
   library.pollingInterval = 12000;
