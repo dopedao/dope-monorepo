3e6d925835d828294e03d4fbadf20afb120ee3c6
Tarrence van As <tarrence13@gmail.com>
2021-09-29 11:43:08 -0700

    fix(web): contract addresses

--- packages/web/common/constants.ts
+++ packages/web/common/constants.ts
@@ -1,8 +1,8 @@
 export const NETWORK = {
   1: {
     contracts: {
-      dope: '0xEf879818335a10Db667810a9B668A8F537389194',
-      paper: '0x7988fCA891B30cF8E23459B09b851F9d79B17215',
+      dope: '0x8707276df042e89669d69a177d3da7dc78bd8723',
+      paper: '0x7ae1d57b58fa6411f32948314badd83583ee0e8c',
       stockpile: '0xd1A506727b8f4221B55C22b2B62B7BE6635E0569',
     },
     rpc: 'https://eth-mainnet.alchemyapi.io/v2/4YF7OoE2seG3X12m9bfIJdiRv2zwUaAx',
