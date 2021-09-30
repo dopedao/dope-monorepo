29191924010b35662d9958ce561969b6cc892038
Tarrence van As <tarrence13@gmail.com>
2021-09-29 19:53:57 -0700

    feat(contracts): render lady

--- packages/contracts/scripts/contract-size.sh
+++ packages/contracts/scripts/contract-size.sh
@@ -5,6 +5,12 @@ set -eo pipefail
 # import the deployment helpers
 . $(dirname $0)/common.sh
 
+
+contract_size=$(contract_size MetadataBuilder)
+echo "Contract Name: MetadataBuilder"
+echo "Contract Size: ${contract_size} bytes"
+echo "$(( 24576 - ${contract_size} )) bytes left to reach the smart contract size limit of 24576 bytes."
+
 contract_size=$(contract_size Components 0x7988fCA891B30cF8E23459B09b851F9d79B17215)
 echo "Contract Name: Components"
 echo "Contract Size: ${contract_size} bytes"
