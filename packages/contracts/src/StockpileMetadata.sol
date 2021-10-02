81838488b6b5aeea638fe366be6b53fdeeb7a15e
Tarrence van As <tarrence13@gmail.com>
2021-10-01 17:22:14 -0700

    feat: support male/female encodings

--- packages/contracts/src/StockpileMetadata.sol
+++ packages/contracts/src/StockpileMetadata.sol
@@ -6,6 +6,11 @@ import './TokenId.sol';
 
 import { MetadataBuilder } from './MetadataBuilder.sol';
 
+library Gender {
+    uint8 internal constant MALE = 0x0;
+    uint8 internal constant FEMALE = 0x1;
+}
+
 /// @title Helper contract for generating ERC-1155 token ids and descriptions for
 /// the individual items inside a Loot bag.
 /// @author Tarrence van As, forked from Georgios Konstantopoulos
@@ -26,7 +31,7 @@ contract StockpileMetadata {
         'Ring'
     ];
 
-    bytes internal constant lady =
+    bytes internal constant female =
         hex'000a26361a050004240300040006240200040007240100020009240100020008240200020002240100052402000300012401000524020006000324030006000224040006000224040004000624020003000824010002000a2402000a2401000224010008240100012402000624010001240100012403000524010001240100012403000424020001240100012403000424020001240100012403000424020001240100012403000424020001240100012402000624010001240224020008240224020006240100012401240300022402000324010004000224020002240200040002240200022402000400022402000224020004000224020002240200040002240200022402000400022402000224020004000224020002240200040002240200022402000400012403000124030004000124030001240300040001240300012403000400012403000124030004000124030001240300040001240300012403000400012403000124030004000124030001240300040001240300012403000400012403000124030003000324020003240100';
     bytes internal constant man =
         hex'000927361907000324040006000524030006000524030006000524030006000524030007000424030007000324040007000224050004000724030002000b24010001000d2401000d2401000d240e240e24022401000b24022401000b24022401000b24022401000b24022401000b24022401000b24022401000b24022401000b24022401000b240c240100012402240100032402000524010003000324020004240200030003240300032402000300032403000324020003000324030003240200030003240300032402000300032403000324020003000324030003240200030003240300032402000300022404000224030003000224040002240300030002240400022403000300022404000224030003000224040002240300030002240400022403000300022404000224030003000224040002240300030002240400022403000300022404000324020003000324030004240100';
@@ -38,7 +43,7 @@ contract StockpileMetadata {
     string[] public backgrounds;
 
     // Item RLE (TokenID => RLE)
-    mapping(uint256 => bytes) internal rles;
+    mapping(uint256 => bytes[2]) internal rles;
 
     Components internal sc;
 
@@ -61,22 +66,27 @@ contract StockpileMetadata {
 
     /// @notice Returns an SVG for the provided token id
     function tokenURI(uint256 tokenId) public view returns (string memory) {
-        bytes[] memory parts = new bytes[](3);
+        bytes[] memory parts = new bytes[](4);
 
         bytes memory man_ = man;
         man_[2] = bytes1(uint8(man_[2]) - uint8(12));
         man_[4] = bytes1(uint8(man_[4]) - uint8(12));
         parts[0] = man_;
 
-        bytes memory rle = tokenRLE(tokenId);
-        rle[2] = bytes1(uint8(rle[2]) - uint8(12));
-        rle[4] = bytes1(uint8(rle[4]) - uint8(12));
-        parts[1] = rle;
+        bytes memory maleRle = tokenRLE(tokenId, Gender.MALE);
+        maleRle[2] = bytes1(uint8(maleRle[2]) - uint8(12));
+        maleRle[4] = bytes1(uint8(maleRle[4]) - uint8(12));
+        parts[1] = maleRle;
 
-        bytes memory lady_ = lady;
-        lady_[2] = bytes1(uint8(lady_[2]) + uint8(12));
-        lady_[4] = bytes1(uint8(lady_[4]) + uint8(12));
-        parts[2] = lady_;
+        bytes memory female_ = female;
+        female_[2] = bytes1(uint8(female_[2]) + uint8(12));
+        female_[4] = bytes1(uint8(female_[4]) + uint8(12));
+        parts[2] = female_;
+
+        bytes memory femaleRle = tokenRLE(tokenId, Gender.FEMALE);
+        femaleRle[2] = bytes1(uint8(femaleRle[2]) - uint8(12));
+        femaleRle[4] = bytes1(uint8(femaleRle[4]) - uint8(12));
+        parts[3] = femaleRle;
 
         (uint8[5] memory components, uint8 componentType) = TokenId.fromId(tokenId);
 
@@ -92,12 +102,12 @@ contract StockpileMetadata {
             );
     }
 
-    function tokenRLE(uint256 id) public view returns (bytes memory) {
+    function tokenRLE(uint256 id, uint8 gender) public view returns (bytes memory) {
         if (rles[id].length > 0) {
-            return rles[id];
+            return rles[id][gender];
         }
 
-        return rles[TokenId.decode(id, 1)];
+        return rles[TokenId.decode(id, 1)][gender];
     }
 
     /// @notice Returns the attributes associated with this item.
