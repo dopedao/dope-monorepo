f7a74c75d1e78625213c5be71011199ccebc26f6
subimage <seth@subimage.com>
2021-10-16 18:40:09 -0700

    Store opensea info on DopeToken document, adjust indexes

--- packages/web/script/fauna-init-database.ts
+++ packages/web/script/fauna-init-database.ts
@@ -14,21 +14,88 @@ const initFromGraphQL = async () => {
   // Override deletes documents and replaces schema in place
   // https://docs.fauna.com/fauna/current/api/graphql/endpoints?lang=javascript
   const url = 'https://graphql.us.fauna.com/import?mode=override';
-  const response = await fetch(url, {
+  await fetch(url, {
     method: 'POST', 
     headers: {
       'Authorization': `Bearer ${FAUNA_KEY}`
     },
     body: schema
   });
-  // console.log(response);
-  // const result = await response.text();
-  // console.log(result);
 };
+
+// Index creation using "values" which is primarily used for sorting
+// https://docs.fauna.com/fauna/current/tutorials/indexes/sort
+const createDopeTokenSortIndex = async (name: string, fieldSort: any) => {
+  console.log(`Creating SORT Index: ${name}`);
+  return await client.query(
+    q.CreateIndex({
+      name: name,
+      source: q.Collection('DopeToken'),
+      values: [fieldSort, { field: ['ref'] }]
+    })
+  );
+}
+
+// Index creation using "terms" which helps searching
+// https://docs.fauna.com/fauna/current/tutorials/indexes/search
+const createDopeTokenSearchIndex = async (name: string, terms: any) => {
+  console.log(`Creating SEARCH Index: ${name}`);
+  return await client.query(
+    q.CreateIndex({
+      name: name,
+      source: q.Collection('DopeToken'),
+      terms: terms
+    })
+  );
+}
 
 (async () => {
   try {
     await initFromGraphQL();
+    
+    console.log('…Sleeping to ensure schema committed to Fauna');
+    await new Promise(resolve => setTimeout(resolve, 5000));
+
+    await createDopeTokenSortIndex(
+      'by-rank', 
+      { field: ['data', 'rank'] }
+    );
+    await createDopeTokenSortIndex(
+      'by-most-affordable',
+      { field: ['data', 'open_sea_current_sale_price_eth'] }
+    );
+    await createDopeTokenSortIndex(
+      'by-highest-last-sale', 
+      {  field: ['data', 'open_sea_last_sale_price_eth'], reverse: true }
+    );
+
+    await createDopeTokenSearchIndex(
+      'item-text',
+      [
+        { field: ['data', 'clothes'] },
+        { field: ['data', 'drugs'] },
+        { field: ['data', 'foot'] },
+        { field: ['data', 'hand'] },
+        { field: ['data', 'neck'] },
+        { field: ['data', 'ring'] },
+        { field: ['data', 'vehicle'] },
+        { field: ['data', 'waist'] },  
+        { field: ['data', 'weapon'] }
+      ]
+    );
+    await createDopeTokenSearchIndex(
+      'claimed',
+      [{ field: ['data', 'claimed'] }]
+    );
+    await createDopeTokenSearchIndex(
+      'unbundled',
+      [{ field: ['data', 'unbundled'] }]
+    );
+    await createDopeTokenSearchIndex(
+      'on-sale',
+      [{ field: ['data', 'open_sea_is_on_sale'] }]
+    );
+
     process.exit(0);
   } catch (e) {
     console.error(e);
--- packages/web/script/fauna-refresh-opensea.ts
+++ packages/web/script/fauna-refresh-opensea.ts
@@ -9,28 +9,35 @@ import { getOpenSeaAssets } from '../src/OpenSeaAsset';
 (async () => {
   try {
     const assets = await getOpenSeaAssets();
-    const assetsWithTokenId = assets.map(asset => [asset.token_id, asset]);
-    const response = await client.query(
+    // Translate to our document store in Fauna
+    const assetsWithTokenId = assets.map(asset => [
+      asset.token_id, 
+      {
+        open_sea_is_on_sale: asset.is_on_sale,
+        open_sea_current_sale_price_eth: asset.current_sale_price_eth,
+        open_sea_last_sale_price_eth: asset.last_sale_price_eth
+      }
+    ]);
+    await client.query(
       q.Map(
         assetsWithTokenId, 
         q.Lambda(
           ['token_id', 'data'], 
           // Fauna doesn't have a native "upsert", so we use an if statement
           q.If(
-            q.Exists(q.Ref(q.Collection('OpenSeaAsset'), q.Var('token_id'))),
+            q.Exists(q.Ref(q.Collection('DopeToken'), q.Var('token_id'))),
             q.Update(
-              q.Ref(q.Collection('OpenSeaAsset'), q.Var('token_id')),
+              q.Ref(q.Collection('DopeToken'), q.Var('token_id')),
               { data: q.Var('data') }
             ),
             q.Create(
-              q.Ref(q.Collection('OpenSeaAsset'), q.Var('token_id')),
+              q.Ref(q.Collection('DopeToken'), q.Var('token_id')),
               { data: q.Var('data') }
             )
           )
         )
       )
     );
-    // console.log(response);
     console.log('Done');
     process.exit(0);
   } catch (e) {
