15b5dfc2e447453e1c25e1de2a75bb16d1771ff6
subimage <seth@subimage.com>
2021-10-19 01:21:58 -0700

    Need additional Indexes that allow for Ref passed in when matching other terms

--- packages/web/fauna/indexes/ByRankIndex.ts
+++ packages/web/fauna/indexes/ByRankIndex.ts
@@ -1,15 +1,33 @@
 import { client, q } from '../../src/fauna-client';
 
+const values = [
+  { field: ['data', 'rank'] },
+  { field: ['ref'] }
+];
+
 // Sort index by Rank ASC (default)
+// 
+// Create standard Index without matching necessary,
+// and another that allows us to combine it with other 
+// indexes for Searching + Sorting as found here:
+//
+// https://docs.fauna.com/fauna/current/tutorials/indexes/search_and_sort
 export const create = async () => {
   return await client.query(
-    q.CreateIndex({
-      name: 'by_rank',
-      source: q.Collection('DopeToken'),
-      values: [
-        { field: ['data', 'rank'] },
-        { field: ['ref'] }
-      ]
-    })
+    q.Do(
+      q.CreateIndex({
+        name: 'by_rank',
+        source: q.Collection('DopeToken'),
+        values: values
+      }),
+      q.CreateIndex({
+        name: 'by_rank_with_ref',
+        source: q.Collection('DopeToken'),
+        values: values,
+        terms: [
+          { field: ['ref'] }
+        ]
+      }),
+    )
   );
 }
