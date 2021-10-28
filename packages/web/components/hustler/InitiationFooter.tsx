9e9e305ec53fc4efd870a16d26dbdfb7ef294907
subimage <seth@subimage.com>
2021-10-28 14:10:58 -0700

    Countdown timer

--- packages/web/components/hustler/InitiationFooter.tsx
+++ packages/web/components/hustler/InitiationFooter.tsx
@@ -1,13 +1,10 @@
+import { css } from '@emotion/react';
 import PanelFooter from '../PanelFooter';
+import styled from '@emotion/styled';
+// https://github.com/ndresx/react-countdown
+import Countdown from 'react-countdown';
 
-const beforeLaunch = true;
 const hasDopeNft = false;
-
-const CountdownTimer = () => {
-  return <>
-    Counting down
-  </>;
-}
 
 const NoDopeMessage = () => {
   return <>
@@ -21,20 +18,63 @@ const MintHustlerControls = () => {
   </>;
 }
 
-const InitiationFooter = () => {
-  let footerContent;
-
-  if (beforeLaunch) {
-    footerContent = <CountdownTimer />;
-  } else if (hasDopeNft) {
-    footerContent = <MintHustlerControls />;    
-  } else {
-    footerContent = <NoDopeMessage />;
+const CountdownWrapper = styled.div`
+  text-align: center;
+  font-size: 2.5em;
+  padding: 8px;
+  span.dots {
+    color: #767674;
   }
+`;
 
+interface CountdownRenderProps {
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+  completed: boolean;
+}
+
+const zeroPad = (i: number) => {
+  let iStr = i.toString();
+  if (i < 10) {
+    iStr = `0${iStr}`;
+  }
+  return iStr;
+}
+
+const countdownRenderer = (
+    { days, hours, minutes, seconds, completed }: 
+    CountdownRenderProps
+  )  => {
+  if (completed) {
+    if (hasDopeNft) {
+      return <MintHustlerControls />;    
+    } else {
+      return <NoDopeMessage />;
+    }
+  } else {
+    return (
+      <CountdownWrapper>
+        {zeroPad(days)}
+        <span className="dots">:</span>
+        {zeroPad(hours)}
+        <span className="dots">:</span>
+        {zeroPad(minutes)}
+        <span className="dots">:</span>
+        {zeroPad(seconds)}
+      </CountdownWrapper>
+    );
+  }
+};
+
+const InitiationFooter = () => {
   return (    
-    <PanelFooter>
-      { footerContent } 
+    <PanelFooter css={css`height:auto;`}>
+      <Countdown
+        date='2021-10-31T01:02:03'
+        renderer={countdownRenderer}
+      />
     </PanelFooter>
   )
 }
