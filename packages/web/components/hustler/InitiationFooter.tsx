2b6faa0f0936c7bb83c4a86a5778553cd7c83fcd
subimage <seth@subimage.com>
2021-10-28 14:54:32 -0700

    No dope footer link to swap meet

--- packages/web/components/hustler/InitiationFooter.tsx
+++ packages/web/components/hustler/InitiationFooter.tsx
@@ -1,14 +1,21 @@
 import { css } from '@emotion/react';
+import { Button } from '@chakra-ui/button';
 import PanelFooter from '../PanelFooter';
 import styled from '@emotion/styled';
 // https://github.com/ndresx/react-countdown
 import Countdown from 'react-countdown';
+import Link from 'next/link';
 
 const hasDopeNft = false;
 
 const NoDopeMessage = () => {
   return <>
-    Buy some dope
+    <div css={css`text-align:center;padding:16px;`}>
+      <p>** NO DOPE IN WALLET **</p>
+      <Link href="/swap-meet?status=For+Sale&sort_by=Most+Affordable">
+        <Button variant='primary'>Shop for DOPE NFTs</Button>
+      </Link>
+    </div>
   </>;
 }
 
@@ -44,9 +51,10 @@ const zeroPad = (i: number) => {
 }
 
 const countdownRenderer = (
-    { days, hours, minutes, seconds, completed }: 
-    CountdownRenderProps
-  )  => {
+  { days, hours, minutes, seconds, completed }: 
+  CountdownRenderProps
+)  => {
+  completed = true;
   if (completed) {
     if (hasDopeNft) {
       return <MintHustlerControls />;    
@@ -54,14 +62,15 @@ const countdownRenderer = (
       return <NoDopeMessage />;
     }
   } else {
+    const d = <span className="dots">:</span>;
     return (
       <CountdownWrapper>
         {zeroPad(days)}
-        <span className="dots">:</span>
+        {d}
         {zeroPad(hours)}
-        <span className="dots">:</span>
+        {d}
         {zeroPad(minutes)}
-        <span className="dots">:</span>
+        {d}
         {zeroPad(seconds)}
       </CountdownWrapper>
     );
