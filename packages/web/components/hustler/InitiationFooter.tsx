0195a95deb7de6be6af74fc92294196c5603a248
subimage <seth@subimage.com>
2021-10-30 15:28:25 -0700

    Show timer unless on TestNet

--- packages/web/components/hustler/InitiationFooter.tsx
+++ packages/web/components/hustler/InitiationFooter.tsx
@@ -2,6 +2,8 @@ import { Button } from '@chakra-ui/button';
 import { css } from '@emotion/react';
 import { Select } from '@chakra-ui/react';
 import { ChangeEvent, useState } from 'react';
+import { useWeb3React } from '@web3-react/core';
+import { zeroPad } from '../../src/utils';
 import PanelFooter from '../PanelFooter';
 import styled from '@emotion/styled';
 // https://github.com/ndresx/react-countdown
@@ -73,7 +75,7 @@ const CountdownWrapper = styled.div`
   font-size: 2.5em;
   padding: 8px;
   span.dots {
-    color: #767674;
+    color: #A8A9AE;
   }
 `;
 
@@ -85,45 +87,43 @@ interface CountdownRenderProps {
   completed: boolean;
 }
 
-const zeroPad = (i: number) => {
-  let iStr = i.toString();
-  if (i < 10) {
-    iStr = `0${iStr}`;
-  }
-  return iStr;
+const countSeparator = (sep: string) => {
+  return <span className="dots">{sep}</span>;
 }
 
 const countdownRenderer = (
   { days, hours, minutes, seconds, completed }: 
   CountdownRenderProps
 )  => {
-  // completed = true;
-  if (completed) {
-    if (hasDopeNft) {
-      return <MintHustlerControls />;    
-    } else {
-      return <NoDopeMessage />;
-    }
-  } else {
-    const d = <span className="dots">:</span>;
-    return (
-      <PanelFooter css={css`height:auto;`}>
-        <CountdownWrapper>
-          {zeroPad(days)}
-          {d}
-          {zeroPad(hours)}
-          {d}
-          {zeroPad(minutes)}
-          {d}
-          {zeroPad(seconds)}
-        </CountdownWrapper>
-      </PanelFooter>
-    );
-  }
+  // Custom Countdown render for style points
+  return (
+    <PanelFooter css={css`height:auto;`}>
+      <CountdownWrapper>
+        {zeroPad(days)}
+        {countSeparator('D')}
+        {zeroPad(hours)}
+        {countSeparator('H')}
+        {zeroPad(minutes)}
+        {countSeparator('M')}
+        {zeroPad(seconds)}
+        {countSeparator('S')}
+      </CountdownWrapper>
+    </PanelFooter>
+  );
 };
 
 const InitiationFooter = () => {
-  return <Countdown date='2021-10-31T01:02:03' renderer={countdownRenderer} />
+  const hustlerMintTime = new Date(2021, 10, 1, 12);
+  const currentTime = new Date();;
+
+  const { chainId } = useWeb3React();
+  const onTestNetOrAfterLaunch = (chainId == 4) || (currentTime >= hustlerMintTime);
+
+  if (onTestNetOrAfterLaunch) {
+    return <InitiationFooterDopeContent />;
+  } else {
+    return <Countdown date={hustlerMintTime} renderer={countdownRenderer} />
+  }
 }
 
 export default InitiationFooter;
