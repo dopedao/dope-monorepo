c3429e3af4d100213ad8d252101ecb921f791254
subimage <seth@subimage.com>
2021-11-05 23:17:41 -0700

    Fix error and randomize hustler attributes

--- packages/web/pages/loot/[id].tsx
+++ packages/web/pages/loot/[id].tsx
@@ -1,17 +1,45 @@
+import styled from '@emotion/styled';
+import { useReactiveVar } from '@apollo/client';
+import { HustlerInitConfig } from '../../src/HustlerInitiation';
 import { useRouter } from 'next/router';
-import AppWindow from '../../components/AppWindow';
+import DesktopWindow from '../../components/DesktopWindow';
 import Head from '../../components/Head';
 import RenderFromLootId from '../../components/hustler/RenderFromLootId';
 
+
 const HustlerFromLoot = () => {
+  const hustlerConfig = useReactiveVar(HustlerInitConfig);
   const router = useRouter();
   const { id } = router.query;
 
+  const title = `Hustler Preview: ${id}`;
+
+  const HustlerContainer = styled.div`
+    background-color: ${hustlerConfig.bgColor};
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-start;
+    & > * {
+      flex: 1;
+    }
+  `;
+  
   return (
-    <AppWindow padBody={false}>
-      <Head title="Hustler Preview" />
-      <RenderFromLootId id={id} />
-    </AppWindow>
+    <DesktopWindow title={title}>
+      <Head title={title} />
+      <HustlerContainer>
+        <RenderFromLootId 
+          id={id?.toString() ?? '1'} 
+          sex={hustlerConfig.sex}
+          body={hustlerConfig.body}
+          hair={hustlerConfig.hair}
+          facialHair={hustlerConfig.facialHair}
+          bgColor={hustlerConfig.bgColor}
+        />
+      </HustlerContainer>
+    </DesktopWindow>
   );
 };
 
