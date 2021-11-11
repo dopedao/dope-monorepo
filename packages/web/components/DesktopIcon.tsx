4e3241dd58f5b16a5366bfc07245d81ea431b35a
Ismail Ghallou <ismai23l@hotmail.com>
2021-11-11 18:45:02 +0100

    styling: add telegram desktop icon (#67)

--- packages/web/components/DesktopIcon.tsx
+++ packages/web/components/DesktopIcon.tsx
@@ -2,77 +2,75 @@ import { css } from '@emotion/react';
 import { media } from '../styles/mixins';
 import Image from 'next/image';
 
-interface IconProps {
+type IconProps = {
   icon: string;
   label: string;
   clickAction: () => void;
-}
+};
 
-const DesktopIcon = ({ icon, label, clickAction }: IconProps) => {
-  return (
+const DesktopIcon = ({ icon, label, clickAction }: IconProps) => (
+  <div
+    onClick={() => clickAction()}
+    className="desktop-icon"
+    css={css`
+      display: flex;
+      justify-content: center;
+      flex-direction: column;
+      cursor: pointer;
+      cursor: hand;
+      // Default
+      width: 96px;
+      height: 96px;
+      ${media.tablet`
+          width: 128px;
+          height: 128px;
+        `}
+      ${media.laptop`
+          width: 128px;
+          height: 128px;
+        `}
+    `}
+  >
     <div
-      onClick={() => clickAction()}
-      className="desktop-icon"
+      className="desktop-icon-image"
       css={css`
+        align-self: center;
         display: flex;
         justify-content: center;
-        flex-direction: column;
-        cursor: pointer;
-        cursor: hand;
+        align-items: center;
+        margin-bottom: 8px;
         // Default
-        width: 96px;
-        height: 96px;
+        width: 48px;
+        height: 48px;
         ${media.tablet`
-          width: 128px;
-          height: 128px;
-        `}
-        ${media.laptop`
-          width: 128px;
-          height: 128px;
-        `}
-      `}
-    >
-      <div
-        className="desktop-icon-image"
-        css={css`
-          align-self: center;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          margin-bottom: 8px;
-          // Default
-          width: 48px;
-          height: 48px;
-          ${media.tablet`
             width: 54px;
             height: 54px;
           `}
-          ${media.laptop`
+        ${media.laptop`
             width: 64px;
             height: 64px;
           `}
           img {
-            display: block;
-          }
-        `}
-      >
-        <Image src={`/images/icon/${icon}.svg`} alt={icon} width="100%" height="100%" />
-      </div>
-      <div
-        className="desktop-icon-label"
-        css={css`
-          align-self: center;
-          font-size: 12px;
-          background-color: #000;
-          color: #fff;
-          text-align: center;
-          padding: 4px 8px;
-        `}
-      >
-        {label}
-      </div>
+          display: block;
+        }
+      `}
+    >
+      <Image src={`/images/icon/${icon}.svg`} alt={icon} width="100%" height="100%" />
     </div>
-  );
-};
+    <div
+      className="desktop-icon-label"
+      css={css`
+        align-self: center;
+        font-size: 12px;
+        background-color: #000;
+        color: #fff;
+        text-align: center;
+        padding: 4px 8px;
+      `}
+    >
+      {label}
+    </div>
+  </div>
+);
 
 export default DesktopIcon;
