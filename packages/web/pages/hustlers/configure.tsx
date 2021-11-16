5573f90a2a9f7c29454b6f9d28fd370502120de5
Ismail Ghallou <ismai23l@hotmail.com>
2021-11-16 23:56:39 +0100

    fix: show balance on the new pages (#77)

--- packages/web/pages/hustlers/configure.tsx
+++ packages/web/pages/hustlers/configure.tsx
@@ -1,7 +1,8 @@
 import { css } from '@emotion/react';
 import { useReactiveVar } from '@apollo/client';
-// import { useWeb3React } from '@web3-react/core';
+import { useWeb3React } from '@web3-react/core';
 import { HustlerInitConfig } from 'src/HustlerConfig';
+import { useWalletQuery } from 'src/generated/graphql';
 // import { HUSTLER_MINT_TIME } from 'src/constants';
 import AppWindow from 'components/AppWindow';
 import ConfigurationControls from 'components/hustler/ConfigurationControls';
@@ -13,12 +14,21 @@ import StackedResponsiveContainer from 'components/StackedResponsiveContainer';
 
 const Hustler = () => {
   const hustlerConfig = useReactiveVar(HustlerInitConfig);
+  const { account } = useWeb3React();
+  const { data, loading } = useWalletQuery({
+    variables: { id: account?.toLowerCase() || '' },
+    skip: !account,
+  });
   // const currentTime = new Date();
   // const { chainId } = useWeb3React();
   // const onTestNetOrAfterHustlerLaunch = chainId == 4 || currentTime >= HUSTLER_MINT_TIME;
 
   return (
-    <AppWindow requiresWalletConnection={true}>
+    <AppWindow
+      requiresWalletConnection={true}
+      balance={data?.wallet?.paper}
+      loadingBalance={loading}
+    >
       <Head title="Create Your Hustler" />
       <StackedResponsiveContainer>
         <PanelContainer
