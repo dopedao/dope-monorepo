9d9b19b2129fd8c6ece9e71cbd639af778b0e4d0
subimage <seth@subimage.com>
2021-11-16 00:08:03 -0800

    Nicer rendering for hair selector when switching between sexes

--- packages/web/components/hustler/HairSelector.tsx
+++ packages/web/components/hustler/HairSelector.tsx
@@ -1,14 +1,12 @@
 import {
-  Flex,
   FormLabel,
+  Box,
+  Flex,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
   NumberInput,
   NumberInputField,
   NumberInputStepper,
-  NumberIncrementStepper,
-  NumberDecrementStepper,
-  Radio,
-  RadioGroup,
-  Spacer,
 } from '@chakra-ui/react';
 import { HustlerInitConfig, HustlerSex, MAX_HAIR, MAX_FACIAL_HAIR } from 'src/HustlerConfig';
 import { useReactiveVar } from '@apollo/client';
@@ -18,40 +16,22 @@ import PanelTitleBar from '../PanelTitleBar';
 
 const HairSelector = () => {
   const hustlerConfig = useReactiveVar(HustlerInitConfig);
+  const isMale = (hustlerConfig.sex == 'male');
 
   return (
     <PanelContainer>
       <PanelTitleBar>Other</PanelTitleBar>
       <PanelBody>
-        <FormLabel htmlFor="hair">Hair</FormLabel>
-        <NumberInput
-          name="hair"
-          defaultValue={0}
-          min={0}
-          max={MAX_HAIR}
-          onChange={value => HustlerInitConfig({ ...hustlerConfig, hair: parseInt(value) })}
-          value={hustlerConfig.hair}
-        >
-          <NumberInputField />
-          <NumberInputStepper>
-            <NumberIncrementStepper />
-            <NumberDecrementStepper />
-          </NumberInputStepper>
-        </NumberInput>
-        {/* Only render Facial Hair for Males  */}
-        {hustlerConfig.sex === 'male' && (
-          <>
-            <br />
-            <FormLabel htmlFor="facial_hair">Facial Hair</FormLabel>
+        <Flex gridGap={4}>
+          <Box width={ isMale ? '50%' : '100%' }>
+            <FormLabel htmlFor="hair">Hair</FormLabel>
             <NumberInput
-              name="facial_hair"
+              name="hair"
               defaultValue={0}
               min={0}
-              max={MAX_FACIAL_HAIR}
-              onChange={value =>
-                HustlerInitConfig({ ...hustlerConfig, facialHair: parseInt(value) })
-              }
-              value={hustlerConfig.facialHair}
+              max={MAX_HAIR}
+              onChange={value => HustlerInitConfig({ ...hustlerConfig, hair: parseInt(value) })}
+              value={hustlerConfig.hair}
             >
               <NumberInputField />
               <NumberInputStepper>
@@ -59,8 +39,30 @@ const HairSelector = () => {
                 <NumberDecrementStepper />
               </NumberInputStepper>
             </NumberInput>
-          </>
-        )}
+          </Box>
+          {/* Only render Facial Hair for Males  */}
+          {isMale && (
+            <Box width="50%">
+              <FormLabel htmlFor="facial_hair">Facial Hair</FormLabel>
+              <NumberInput
+                name="facial_hair"
+                defaultValue={0}
+                min={0}
+                max={MAX_FACIAL_HAIR}
+                onChange={value =>
+                  HustlerInitConfig({ ...hustlerConfig, facialHair: parseInt(value) })
+                }
+                value={hustlerConfig.facialHair}
+              >
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            </Box>
+          )}
+        </Flex>
       </PanelBody>
     </PanelContainer>
   );
