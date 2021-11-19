3aa595705b9424cbefce8d3316fb4e4001ac06e2
Ismail Ghallou <ismai23l@hotmail.com>
2021-11-19 03:09:32 +0100

    styling: handle loading state while txn is ongoing and handle rejection (#97)

---
+++ packages/web/svg/Spinner/index.tsx
@@ -0,0 +1,9 @@
+import { Wrapper } from './styles';
+
+const Spinner = () => (
+  <Wrapper width="24" height="24" viewBox="0 0 50 50" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="25" cy="25" r="20" fill="none" strokeWidth="5"></circle>
+  </Wrapper>
+);
+
+export default Spinner;
