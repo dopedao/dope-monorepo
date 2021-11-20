3dc03bc9dbab4823257f5a761c9d1c25b2a65641
Tarrence van As <tarrencev@users.noreply.github.com>
2021-11-19 19:58:32 -0800

    Feat/approvalslogic (#114)
    * feat: gate mint if paper not approved or not enough
    
    * feat: move countdown to mint

--- packages/web/components/hustler/InitiationFooter.tsx
+++ packages/web/components/hustler/InitiationFooter.tsx
@@ -1,57 +1,8 @@
-import { css } from '@emotion/react';
-import { zeroPad } from 'src/utils';
 import { HustlerInitConfig, getRandomHustler } from 'src/HustlerConfig';
 import { useEffect, useState } from 'react';
-import PanelFooter from 'components/PanelFooter';
-import styled from '@emotion/styled';
-// https://github.com/ndresx/react-countdown
-import Countdown from 'react-countdown';
 import InitiationFooterDopeContent from './InitiationFooterDopeContent';
 import { useLastestBlock } from 'hooks/web3';
 import { useReleaseDate } from 'hooks/contracts';
-
-const CountdownWrapper = styled.div`
-  text-align: center;
-  font-size: 2.5em;
-  padding: 8px;
-  span.dots {
-    color: #a8a9ae;
-  }
-`;
-
-interface CountdownRenderProps {
-  days: number;
-  hours: number;
-  minutes: number;
-  seconds: number;
-  completed: boolean;
-}
-
-const countSeparator = (sep: string) => {
-  return <span className="dots">{sep}</span>;
-};
-
-const countdownRenderer = ({ days, hours, minutes, seconds, completed }: CountdownRenderProps) => {
-  // Custom Countdown render for style points
-  return (
-    <PanelFooter
-      css={css`
-        height: auto;
-      `}
-    >
-      <CountdownWrapper>
-        {zeroPad(days)}
-        {countSeparator('D')}
-        {zeroPad(hours)}
-        {countSeparator('H')}
-        {zeroPad(minutes)}
-        {countSeparator('M')}
-        {zeroPad(seconds)}
-        {countSeparator('S')}
-      </CountdownWrapper>
-    </PanelFooter>
-  );
-};
 
 const InitiationFooter = () => {
   const releaseDate = useReleaseDate();
@@ -76,14 +27,7 @@ const InitiationFooter = () => {
     return () => clearInterval(randomHustlerRenderInterval);
   }, []);
 
-  if (isLaunched) {
-    clearInterval(randomHustlerRenderInterval);
-    return <InitiationFooterDopeContent />;
-  } else if (releaseDate) {
-    return <Countdown date={releaseDate} renderer={countdownRenderer} />;
-  } else {
-    return <div>loading</div>;
-  }
+  return <InitiationFooterDopeContent />;
 };
 
 export default InitiationFooter;
