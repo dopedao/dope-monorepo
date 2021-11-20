14344ccc28be30047399d5d1bd28c3cb8b35ed96
subimage <seth@subimage.com>
2021-11-19 17:22:18 -0800

    Link to minting faq

--- packages/web/components/StickyNoteHustlerMint.tsx
+++ packages/web/components/StickyNoteHustlerMint.tsx
@@ -2,20 +2,26 @@ import { useReleaseDate } from "hooks/contracts";
 import Countdown from "react-countdown";
 import { Link } from "@chakra-ui/layout";
 import StickyNote from "./StickyNote";
+import { css } from "@emotion/react";
 
 const StickyNoteHustlerMint = () => {
   const releaseDate = useReleaseDate();
 
   return (
     <StickyNote maxWidth="312px">
-      <h4>
+      <h3>
         <Link href="/hustlers/initiate" variation="primary">
           <a className="primary">
-            👉 Mint your OG Hustler here 👈
+            👉 Mint OG Hustler Here 👈
           </a>
         </Link>
-      </h4>
-      ( <Countdown date={releaseDate ?? Date.now()} /> )
+      </h3>
+      <hr css={css`margin: 1em 0; border-color: black;`} />
+      <span>
+        <a href="https://dope-wars.notion.site/Hustler-Minting-and-Unbundling-25c6dfb9dca64196aedf8def6297c51a" target="hustler-minting-faq" className="primary">
+        Minting FAQ
+        </a>
+      </span>
     </StickyNote>
   );
 };
