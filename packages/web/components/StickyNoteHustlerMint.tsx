9b9e3e838c7d636fa947a1f95cd63cc52379c1b4
subimage <seth@subimage.com>
2021-11-20 23:34:02 -0800

    Nav to and from gangsta party

--- packages/web/components/StickyNoteHustlerMint.tsx
+++ packages/web/components/StickyNoteHustlerMint.tsx
@@ -1,8 +1,19 @@
 import { Link } from '@chakra-ui/layout';
 import StickyNote from './StickyNote';
 import { css } from '@emotion/react';
+import { useRouter } from 'next/router';
+
+const hr = <hr
+  css={css`
+    margin: 1em 0;
+    border-color: rgba(0,0,0,.125);
+  `}
+/>
 
 const StickyNoteHustlerMint = () => {
+  const router = useRouter();
+  const currentPageIsGangsta = router.pathname == '/gangsta-party';
+
   return (
     <StickyNote maxWidth="312px">
       <h3>
@@ -10,12 +21,7 @@ const StickyNoteHustlerMint = () => {
           <a className="primary">👉 Mint Your Hustler Here 👈</a>
         </Link>
       </h3>
-      <hr
-        css={css`
-          margin: 1em 0;
-          border-color: black;
-        `}
-      />
+      { hr }
       <span>
         <a
           href="https://dope-wars.notion.site/Hustler-Minting-and-Unbundling-25c6dfb9dca64196aedf8def6297c51a"
@@ -25,6 +31,16 @@ const StickyNoteHustlerMint = () => {
           Minting FAQ
         </a>
       </span>
+      { !currentPageIsGangsta &&
+        <>
+          { hr }
+          <span>
+            <a href="/gangsta-party" className="primary">
+              Gangsta Party
+            </a>
+          </span>
+        </>
+      }
     </StickyNote>
   );
 };
