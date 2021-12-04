821fba761bba78aad09e2c566c7f048d1a4dc5e7
Tarrence van As <tarrence@vanas.family>
2021-12-04 14:57:55 -0500

    feat(api): wire up sync engine

--- packages/api/processors/dope.go
+++ packages/api/processors/dope.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/dopedao/dope-monorepo/packages/api/contracts/bindings"
+	"github.com/dopedao/dope-monorepo/packages/api/ent"
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
@@ -11,16 +12,18 @@ import (
 
 type DopeProcessor struct {
 	bindings.UnimplementedDopeProcessor
+	ent *ent.Client
 }
 
 func (p *DopeProcessor) Setup(address common.Address, eth interface {
-	ethereum.ChainReader
 	bind.ContractBackend
-}) error {
+	ethereum.ChainReader
+}, ent *ent.Client) error {
 	if err := p.UnimplementedDopeProcessor.Setup(address, eth); err != nil {
 		return err
 	}
 
+	p.ent = ent
 	return nil
 }
 
