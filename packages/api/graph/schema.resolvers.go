e6cc5b22eb26100044bce093578ed6f32d9b4f03
Tarrence van As <tarrence@vanas.family>
2021-12-05 17:53:39 -0500

    fix(api): bind items

--- packages/api/graph/schema.resolvers.go
+++ packages/api/graph/schema.resolvers.go
@@ -5,57 +5,16 @@ package graph
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/dopedao/dope-monorepo/packages/api/ent"
 	generated1 "github.com/dopedao/dope-monorepo/packages/api/graph/generated"
 )
 
-func (r *dopeResolver) Clothes(ctx context.Context, obj *ent.Dope) (string, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-
-func (r *dopeResolver) Foot(ctx context.Context, obj *ent.Dope) (string, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-
-func (r *dopeResolver) Hand(ctx context.Context, obj *ent.Dope) (string, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-
-func (r *dopeResolver) Neck(ctx context.Context, obj *ent.Dope) (string, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-
-func (r *dopeResolver) Ring(ctx context.Context, obj *ent.Dope) (string, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-
-func (r *dopeResolver) Waist(ctx context.Context, obj *ent.Dope) (string, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-
-func (r *dopeResolver) Weapon(ctx context.Context, obj *ent.Dope) (string, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-
-func (r *dopeResolver) Drugs(ctx context.Context, obj *ent.Dope) (string, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-
-func (r *dopeResolver) Vehicle(ctx context.Context, obj *ent.Dope) (string, error) {
-	panic(fmt.Errorf("not implemented"))
-}
-
 func (r *queryResolver) Wallets(ctx context.Context) ([]*ent.Wallet, error) {
 	return r.client.Wallet.Query().All(ctx)
 }
 
-// Dope returns generated1.DopeResolver implementation.
-func (r *Resolver) Dope() generated1.DopeResolver { return &dopeResolver{r} }
-
 // Query returns generated1.QueryResolver implementation.
 func (r *Resolver) Query() generated1.QueryResolver { return &queryResolver{r} }
 
-type dopeResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
