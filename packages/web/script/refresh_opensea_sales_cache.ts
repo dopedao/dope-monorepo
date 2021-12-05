ff0636a6e880600a4c32d515adbf7b59204da9c4
Mayur Chougule <mayurchougule@gmail.com>
2021-12-05 17:18:46 -0500

    Update fauna db from opensea on every run

--- packages/web/script/refresh_opensea_sales_cache.ts
+++ packages/web/script/refresh_opensea_sales_cache.ts
@@ -3,6 +3,7 @@ dotenv.config({ path: __dirname + '/../../../.env' });
 
 import { getOpenSeaAssets } from '../src/OpenSeaAsset';
 import S3 from 'aws-sdk/clients/s3';
+import { client, q } from '../src/fauna_client';
 
 const S3_ID = process.env.S3_ID;
 const S3_SECRET = process.env.S3_SECRET;
@@ -16,6 +17,40 @@ const s3client = new S3({
   secretAccessKey: S3_SECRET,
 });
 
+export const updateOpenseaAssets = async (assetJson: any) => {
+  // Translate to our document store in Fauna
+  const assetsWithTokenId = assetJson.map((
+      asset: { token_id: any; is_on_sale: any; current_sale_price_eth: any; last_sale_price_eth: any;
+    }) => [
+    asset.token_id, 
+    {
+      open_sea_is_on_sale: asset.is_on_sale,
+      open_sea_current_sale_price_eth: asset.current_sale_price_eth,
+      open_sea_last_sale_price_eth: asset.last_sale_price_eth
+    }
+  ]);
+  await client.query(
+    q.Map(
+      assetsWithTokenId, 
+      q.Lambda(
+        ['token_id', 'data'], 
+        // Fauna doesn't have a native "upsert", so we use an if statement
+        q.If(
+          q.Exists(q.Ref(q.Collection('DopeToken'), q.Var('token_id'))),
+          q.Update(
+            q.Ref(q.Collection('DopeToken'), q.Var('token_id')),
+            { data: q.Var('data') }
+          ),
+          q.Create(
+            q.Ref(q.Collection('DopeToken'), q.Var('token_id')),
+            { data: q.Var('data') }
+          )
+        )
+      )
+    )
+  );
+};
+
 const main = async () => {
   try {
     const assetJson = await getOpenSeaAssets();
@@ -25,6 +60,7 @@ const main = async () => {
       Body: JSON.stringify(assetJson),
     };
     await s3client.upload(params).promise();
+    await updateOpenseaAssets(assetJson);
     process.exit(0);
   } catch (e) {
     console.log(e);
