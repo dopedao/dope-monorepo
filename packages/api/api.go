17a8c602f520949abc258dccd393ea68c2829e0f
Tarrence van As <tarrence@vanas.family>
2021-12-06 10:08:12 -0500

    feat(api): hook up paper processor

--- packages/api/api.go
+++ packages/api/api.go
@@ -3,7 +3,6 @@ package api
 import (
 	"context"
 	"net/http"
-	"reflect"
 	"time"
 
 	"entgo.io/ent/dialect/sql"
@@ -39,11 +38,20 @@ func NewServer(db *sql.Driver) (http.Handler, error) {
 
 	eth := NewEngine(client, "https://eth-kovan.alchemyapi.io/v2/imTJSp6gKyrAIFPFrQRXy1lD087y3FN-", Config{
 		Interval: time.Second * 5,
-		Contracts: []Contract{{
-			Address:    common.HexToAddress("0xd2761Ee62d8772343070A5dE02C436F788EdF60a"),
-			StartBlock: 28278714,
-			Interface:  reflect.TypeOf(processors.DopeProcessor{}),
-		}},
+		Contracts: []Contract{
+			// DOPE
+			{
+				Address:    common.HexToAddress("0xd2761Ee62d8772343070A5dE02C436F788EdF60a"),
+				StartBlock: 28278714,
+				Processor:  new(processors.DopeProcessor),
+			},
+			// PAPER
+			{
+				Address:    common.HexToAddress("0x7ae1d57b58fa6411f32948314badd83583ee0e8c"),
+				StartBlock: 13162150,
+				Processor:  new(processors.PaperProcessor),
+			},
+		},
 	})
 
 	go eth.Sync(context.Background())
