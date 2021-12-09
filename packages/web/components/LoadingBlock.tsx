2eac01f50a08518849a0beac2a0d6a706443d668
subimage <seth@subimage.com>
2021-12-09 08:33:07 -1000

    Make loading bars visible on customizer

--- packages/web/components/LoadingBlock.tsx
+++ packages/web/components/LoadingBlock.tsx
@@ -6,45 +6,46 @@ import { css } from '@emotion/react';
 import styled from '@emotion/styled';
 import { getRandomNumber } from '../src/utils';
 
-const Container = styled.div`
-  height: 100%;
-  flex-direction: column;
-  & > div {
-    background-color: #000;
-    opacity: 0.1;
-    height: 32px;
-    margin-bottom: 8px;
-    float: left;
-    clear: both;
-    @keyframes pulse {
-      0% {
-        opacity: 0.25;
-        width: 10%;
-      }
-      25% {
-        opacity: 0.225;
-        width: 50%;
-      }
-      50% {
-        opacity: 0.3;
-        width: 50%;
-      }
-      75% {
-        opacity: 0.4;
-        width: 60%;
-      }
-      100% {
-        opacity: 0.75;
-        width: 90%;
-      }
-    }
-    animation: pulse 0.75s steps(4, end) 0s infinite;
-  }
-`;
-
-const LoadingBlock = ({ maxRows = 20 }: { maxRows?: number }) => {
+const LoadingBlock = ({ maxRows = 20, color = 'black' }: { maxRows?: number, color?: string }) => {
   const randomRowLength = getRandomNumber(2, maxRows);
   const rows = [];
+
+  const Container = styled.div`
+    height: 100%;
+    flex-direction: column;
+    & > div {
+      background-color: ${color};
+      opacity: 0.1;
+      height: 32px;
+      margin-bottom: 8px;
+      float: left;
+      clear: both;
+      @keyframes pulse {
+        0% {
+          opacity: 0.25;
+          width: 10%;
+        }
+        25% {
+          opacity: 0.225;
+          width: 50%;
+        }
+        50% {
+          opacity: 0.3;
+          width: 50%;
+        }
+        75% {
+          opacity: 0.4;
+          width: 60%;
+        }
+        100% {
+          opacity: 0.75;
+          width: 90%;
+        }
+      }
+      animation: pulse 0.75s steps(4, end) 0s infinite;
+    }
+  `;
+
   for (let i = 0; i < randomRowLength; i++) {
     rows.push(
       <div
