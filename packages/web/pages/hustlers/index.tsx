b4d3089990e65cc4cca3b75526eb19487e9ab9e6
faces-of-eth <facesofeth@proton.me>
2021-12-09 09:15:59 -1000

    Fix color of loader

--- packages/web/pages/hustlers/index.tsx
+++ packages/web/pages/hustlers/index.tsx
@@ -36,7 +36,7 @@ const Container = styled.div`
 
 const ContentLoading = (
   <Container>
-    <LoadingBlock />
+    <LoadingBlock color="white" maxRows={10} />
   </Container>
 );
 const ContentEmpty = (
