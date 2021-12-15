367fa4ed28bb023ff8e7f3f00217b97ab9c6175c
Ismail Ghallou <ismai23l@hotmail.com>
2021-12-15 23:26:06 +0100

    Chore/show success message after customize (#158)
    * fix: show og title for og hustlers only
    
    * styling: handle wording when transaction is processing and wait for it
    
    * styling: show success message after customizing your hustler
    
    * styling: break lines into 2 on the sticky note and force wallet query on hustlers page

--- packages/web/src/pages/hustlers/index.tsx
+++ packages/web/src/pages/hustlers/index.tsx
@@ -1,10 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
+import { useState } from 'react';
 import { Button } from '@chakra-ui/react';
 import Link from 'next/link';
 import styled from '@emotion/styled';
 import { useWeb3React } from '@web3-react/core';
+import { css } from '@emotion/react';
+import { useRouter } from 'next/router';
+import { CloseButton } from '@chakra-ui/close-button';
 import { media } from 'ui/styles/mixins';
-import { useHustlersWalletQuery } from 'generated/graphql';
+import { useHustlersWalletQuery, useWalletQuery } from 'generated/graphql';
 import { useOptimismClient } from 'components/EthereumApolloProvider';
 import AppWindow from 'components/AppWindow';
 import DopeWarsExeNav from 'components/DopeWarsExeNav';
@@ -12,6 +16,7 @@ import Head from 'components/Head';
 import LoadingBlock from 'components/LoadingBlock';
 import RenderFromChain from 'components/hustler/RenderFromChain';
 import Dialog from 'components/Dialog';
+import StickyNote from 'components/StickyNote';
 
 const brickBackground = "#000000 url('/images/tile/brick-black.png') center/25% fixed";
 const streetBackground =
@@ -48,8 +53,8 @@ const ContentEmpty = (
   >
     <p>Hustlers are the in-game and in-ecosystem characters in the Dope Wars universe.</p>
     <p>
-      When you Initiate a Hustler, you receive a customizable profile pic in the form of a new NFT. This
-      image can be customized at any time, and all the artwork is stored on-chain.
+      When you Initiate a Hustler, you receive a customizable profile pic in the form of a new NFT.
+      This image can be customized at any time, and all the artwork is stored on-chain.
     </p>
     <Link href="/dope" passHref>
       <Button variant="primary">Initiate a Hustler</Button>
@@ -58,6 +63,8 @@ const ContentEmpty = (
 );
 
 const Hustlers = () => {
+  const [showSuccessAlert, setShowSuccessAlert] = useState(true);
+  const router = useRouter();
   const { account } = useWeb3React();
   const client = useOptimismClient();
   const { data, loading } = useHustlersWalletQuery({
@@ -65,12 +72,50 @@ const Hustlers = () => {
     variables: { id: account?.toLowerCase() || '' },
     skip: !account,
   });
+  const { loading: walletLoading } = useWalletQuery({
+    variables: { id: account?.toLowerCase() || '' },
+    skip: !account,
+  });
+
+  const handleSuccessAlert = () => {
+    setShowSuccessAlert(false);
+    router.replace('/hustlers');
+  };
 
   return (
     <AppWindow padBody={false} navbar={<DopeWarsExeNav />}>
       <Head title="Your Hustler Squad" />
-      {loading && ContentLoading}
-      {!loading && data?.wallet?.hustlers && data?.wallet?.hustlers.length > 0 && (
+      {account && router.query.c === 'true' && showSuccessAlert && (
+        <StickyNote>
+          <div
+            css={css`
+              display: flex;
+              align-items: center;
+              width: 560px;
+            `}
+          >
+            <p
+              css={css`
+                margin-right: 10px;
+                padding-bottom: unset;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                display: -webkit-box;
+                -webkit-line-clamp: 2; /* number of lines to show */
+                line-clamp: 2;
+                -webkit-box-orient: vertical;
+              `}
+            >
+              {
+                "You've successfully customized your Hustler! It might take a minute for the changes to be reflected."
+              }
+            </p>{' '}
+            <CloseButton onClick={handleSuccessAlert} />
+          </div>
+        </StickyNote>
+      )}
+      {(loading || walletLoading) && ContentLoading}
+      {!loading && !walletLoading && data?.wallet?.hustlers && data?.wallet?.hustlers.length > 0 && (
         <Container>
           <div className="hustlerGrid">
             {data.wallet.hustlers.map(({ id, data }) => {
@@ -88,7 +133,10 @@ const Hustlers = () => {
           </div>
         </Container>
       )}
-      {!loading && (!data?.wallet?.hustlers || data?.wallet?.hustlers.length === 0) && ContentEmpty}
+      {!loading &&
+        !walletLoading &&
+        (!data?.wallet?.hustlers || data?.wallet?.hustlers.length === 0) &&
+        ContentEmpty}
     </AppWindow>
   );
 };
