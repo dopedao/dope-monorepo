90437fc91ccf1657a4fe72b601ea53787c1f2ced
Tarrence van As <tarrence@vanas.family>
2021-12-22 10:17:36 -0500

    fix(api): dope order nil pointer

--- packages/api/graph/schema.resolvers.go
+++ packages/api/graph/schema.resolvers.go
@@ -23,8 +23,9 @@ func (r *queryResolver) Wallets(ctx context.Context, after *ent.Cursor, first *i
 }
 
 func (r *queryResolver) Dopes(ctx context.Context, after *ent.Cursor, first *int, before *ent.Cursor, last *int, orderBy *ent.DopeOrder, where *ent.DopeWhereInput) (*ent.DopeConnection, error) {
-	if orderBy.Field == nil {
+	if orderBy == nil {
 		orderBy.Field = ent.DopeOrderFieldOrder
+		orderBy.Direction = ent.OrderDirectionAsc
 	}
 	return r.client.Dope.Query().Paginate(ctx, after, first, before, last, ent.WithDopeOrder(orderBy), ent.WithDopeFilter(where.Filter))
 }
