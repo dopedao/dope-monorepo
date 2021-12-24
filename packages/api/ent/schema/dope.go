cd75d2e8169ff1bc1393b48128cb6afccba57045
Mayur Chougule <mayurchougule@gmail.com>
2021-12-24 13:06:16 -0500

    Edge renamed to last_sale

--- packages/api/ent/schema/dope.go
+++ packages/api/ent/schema/dope.go
@@ -39,7 +39,7 @@ func (Dope) Fields() []ent.Field {
 func (Dope) Edges() []ent.Edge {
 	return []ent.Edge{
 		edge.From("wallet", Wallet.Type).Ref("dopes").Unique(),
-		edge.From("lastSale", Listing.Type).Ref("dope_lastsales").Unique(),
+		edge.From("last_sale", Listing.Type).Ref("dope_lastsales").Unique(),
 		edge.To("listings", Listing.Type).Annotations(entgql.Bind()),
 		edge.To("items", Item.Type).Annotations(entgql.Bind()),
 	}
