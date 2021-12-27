8716c5b5c3bb83355abd1e2a5f49382af93ea975
Larko <59736843+Larkooo@users.noreply.github.com>
2021-12-27 18:17:01 -0500

    Game/gameplay (#172)
    * added pathfinding class for hustlers and modified to work with multiple objects
    
    * player now inherits from hustler and processes its own inputs
    
    * added hustleranimator class for hustler, to manage the animations
    
    * refactor: using world space coordinates for PF movements
    
    * make path less "optimized"
    
    * updated file name from characteranimations to hustleranimations
    
    * started working on NPCs and modified zone's body to be a sensor
    
    * started working on npcs, inventory, items and refactored some bits of code
    
    * made inventory nullable
    
    * disabled diagonal pathfinding for now + added ellipse collider
    
    * added sensor to player for interacting with npcs, points of interest etc...
    
    * correctly updating sensor position
    
    * applied variable name change across all files
    
    * added last direction field for interaction sensor and other futur uses
    
    * normalize and scale velocity to prevent speed gaining when going in diagonal
    
    * fixed cast mistake

---
+++ packages/web/src/game/anims/HustlerAnimator.ts
@@ -0,0 +1,28 @@
+import Hustler, { Direction } from "game/entities/Hustler";
+
+export default class HustlerAnimator
+{
+    private hustler: Hustler;
+
+    constructor(hustler: Hustler)
+    {
+        this.hustler = hustler;
+    }
+
+    update()
+    {
+        if (this.hustler.moveDirection === Direction.None)
+        {
+            // reset to the first frame of the anim
+            if (this.hustler.anims.currentAnim && !this.hustler.anims.currentFrame.isLast)
+                this.hustler.anims.setCurrentFrame(this.hustler.anims.currentAnim.getLastFrame());
+            this.hustler.stopAfterDelay(100);
+
+            this.hustler.model.stopSpritesAnim();
+            return;
+        }
+
+        this.hustler.play(this.hustler.texture.key + this.hustler.moveDirection, true);
+        this.hustler.model.updateSprites(false, this.hustler.moveDirection);
+    }
+}
--- packages/web/src/game/world/Zone.ts
+++ packages/web/src/game/world/Zone.ts
@@ -1,40 +1,44 @@
 import Hustler from "game/entities/Hustler";
+import EventHandler from "game/handlers/EventHandler";
 
 export default class Zone
 {
     private body: MatterJS.BodyType;
     private scene: Phaser.Scene;
-    private object: Phaser.GameObjects.GameObject;
+    private objects: Array<Phaser.GameObjects.GameObject>;
 
     private inside: boolean = false;
 
     // callbacks
-    private onEnter: () => void;
-    private onExit: () => void;
+    private onEnter?: () => void;
+    private onExit?: () => void;
 
-    constructor(body: MatterJS.BodyType, scene: Phaser.Scene, object: Phaser.GameObjects.GameObject, onEnter: () => void, onExit: () => void)
+    constructor(body: MatterJS.BodyType, scene: Phaser.Scene, objects: Array<Phaser.GameObjects.GameObject>, onEnter?: () => void, onExit?: () => void)
     {
         this.body = body;
+
+        // set the body as being a sensor so that it doesn't collide with anything
+        this.body.isSensor = true;
+
         this.scene = scene;
-        this.object = object;
+        this.objects = objects;
 
         this.onEnter = onEnter;
         this.onExit = onExit;
-
-        // disable collision
-        this.body.collisionFilter.mask = 0;
     }
 
     update()
     {
-        if (!this.inside && this.scene.matter.overlap(this.body, [this.object]))
+        if (!this.inside && this.scene.matter.overlap(this.body, this.objects))
         {
-            this.onEnter();
+            if (this.onEnter)
+                this.onEnter();
             this.inside = true;
         }
-        else if (this.inside && !this.scene.matter.overlap(this.body, [this.object]))
+        else if (this.inside && !this.scene.matter.overlap(this.body, this.objects))
         {
-            this.onExit();
+            if (this.onExit)
+                this.onExit();
             this.inside = false;
         }
     }
