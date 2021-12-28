e8cae75be371433af2f667779306b461f5bf4ffd
Tarrence van As <tarrencev@users.noreply.github.com>
2021-12-28 11:54:57 -0800

    feat(api): index dope paper claim status (#174)

--- packages/api/processors/paper.go
+++ packages/api/processors/paper.go
@@ -35,5 +35,38 @@ func (p *PaperProcessor) ProcessTransfer(ctx context.Context, e *bindings.PaperT
 		}
 	}
 
+	if e.From == (common.Address{}) {
+		loot, err := p.Contract.Loot(nil)
+		if err != nil {
+			return fmt.Errorf("getting loot address: %w", err)
+		}
+
+		dope, err := bindings.NewDope(loot, p.Eth)
+		if err != nil {
+			return fmt.Errorf("initializing dope contract: %w", err)
+		}
+
+		bal, err := dope.BalanceOf(nil, e.To)
+		if err != nil {
+			return fmt.Errorf("getting dope balance: %w", err)
+		}
+
+		for i := 0; i < int(bal.Int64()); i++ {
+			id, err := dope.TokenOfOwnerByIndex(nil, e.To, big.NewInt(int64(i)))
+			if err != nil {
+				return fmt.Errorf("token of owner by index: %w", err)
+			}
+
+			claimed, err := p.Contract.ClaimedByTokenId(nil, id)
+			if err != nil {
+				return fmt.Errorf("claimed by token id: %w", err)
+			}
+
+			if err := tx.Dope.UpdateOneID(id.String()).SetClaimed(claimed).Exec(ctx); err != nil {
+				return fmt.Errorf("updating dope claimed status: %w", err)
+			}
+		}
+	}
+
 	return nil
 }
