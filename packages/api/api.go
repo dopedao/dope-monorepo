18713c2ee944e36476e90ed16571eecdb5eebd69
Tarrence van As <tarrence@vanas.family>
2022-01-01 15:16:43 -0500

    feat(api): index item names for fulltext search

--- packages/api/api.go
+++ packages/api/api.go
@@ -2,7 +2,9 @@ package api
 
 import (
 	"context"
+	"fmt"
 	"net/http"
+	"strings"
 
 	"entgo.io/ent/dialect/sql"
 
@@ -15,13 +17,33 @@ import (
 	"github.com/dopedao/dope-monorepo/packages/api/graph"
 )
 
-func NewServer(ctx context.Context, db *sql.Driver, index bool, network string) (http.Handler, error) {
-	client := ent.NewClient(ent.Driver(db))
+const ts_migation = `
+ALTER TABLE items ADD COLUMN ts tsvector 
+    GENERATED ALWAYS AS 
+        (to_tsvector('english', coalesce((trim(both ' ' from (
+     ' ' || coalesce(name_prefix, '') || 
+     ' ' || coalesce(name_suffix, '') ||
+     ' ' || coalesce(name, '') ||
+     ' ' || coalesce(suffix, '') ||
+     ' ' || coalesce(case when "augmented" then '+1' end, ''))
+    )), ''))) STORED;
+
+CREATE INDEX ts_idx ON items USING GIN (ts);
+`
+
+func NewServer(ctx context.Context, drv *sql.Driver, index bool, network string) (http.Handler, error) {
+	client := ent.NewClient(ent.Driver(drv))
 
 	if index {
 		// Run the auto migration tool.
 		if err := client.Schema.Create(ctx); err != nil {
 			return nil, err
+		}
+
+		if _, err := drv.DB().Exec(ts_migation); err != nil {
+			if !strings.Contains(err.Error(), "already exists") {
+				return nil, fmt.Errorf("applying ts migration: %w", err)
+			}
 		}
 	}
 
