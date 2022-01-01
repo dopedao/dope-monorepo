4c7ded7f9c68a495e5c74a35ff19b38d70dcf5a6
Larko <59736843+Larkooo@users.noreply.github.com>
2021-12-31 21:27:06 -0500

    add toast manager to phaser UI scene

--- packages/web/src/game/scenes/UI.ts
+++ packages/web/src/game/scenes/UI.ts
@@ -1,16 +1,21 @@
+import { isThisMonth } from "date-fns";
 import Citizen from "game/entities/citizen/Citizen";
 import Conversation from "game/entities/citizen/Conversation";
 import Player from "game/entities/Player";
 import EventHandler, { Events } from "game/handlers/EventHandler";
+import Quest from "game/quests/Quest";
+import ToastManager from "game/ui/react/components/ToastManager";
 import DialogueTextBox from "game/ui/rex/DialogueTextBox";
 import { createTextBox } from "game/ui/rex/RexUtils";
 import SpeechBubbleTextBox from "game/ui/rex/SpeechBubbleTextBox";
 import { Scene } from "phaser";
+import { ComponentManager } from "phaser3-react/src/manager";
 import RexUIPlugin from 'phaser3-rex-plugins/templates/ui/ui-plugin';
 import GameScene from "./Game";
 
 export default class UIScene extends Scene {
     public rexUI!: RexUIPlugin;
+    public toastComponent!: ComponentManager;;
 
     public player!: Player;
 
@@ -26,10 +31,11 @@ export default class UIScene extends Scene {
     }
   
     create(): void {
+        this.toastComponent = this.add.reactDom(ToastManager);
         this._handleEvents();
     }
 
-    private _handleEvents()
+    private _handleInteractions()
     {
         EventHandler.emitter().on(Events.PLAYER_INTERACT_NPC, (npc: Citizen) => {
             // open textbox
@@ -79,6 +85,22 @@ export default class UIScene extends Scene {
                 //     });
             }
         });
+    }
+
+    private handleQuests()
+    {
+        // handle quest events
+        EventHandler.emitter().on(Events.PLAYER_NEW_QUEST, (quest: Quest) => {
+            
+        })
+        EventHandler.emitter().on(Events.PLAYER_COMPLETE_QUEST, (quest: Quest) => {
+            
+        });
+    }
+
+    private _handleEvents()
+    {
+        this._handleInteractions();
     }
 }
   
