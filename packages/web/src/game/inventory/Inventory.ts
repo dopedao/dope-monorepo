7b0f5f1f72e9a90078eb74f939f7eb0001f89b9d
Larko <59736843+Larkooo@users.noreply.github.com>
2022-01-05 15:49:03 -0500

    refactor: added helper methods to inventory and added react inventorycomponent

--- packages/web/src/game/inventory/Inventory.ts
+++ packages/web/src/game/inventory/Inventory.ts
@@ -15,6 +15,49 @@ export default class Inventory
             this._items = items;
     }
 
+    // swap items
+    swap(item1: Item | number, item2: Item | number)
+    {
+        const item1Index = item1 instanceof Item ? this._items.indexOf(item1) : item1;
+        const item2Index = item2 instanceof Item ? this._items.indexOf(item2) : item2;
+
+        const temp = this._items[item1Index];
+        this._items[item1Index] = this._items[item2Index];
+        this._items[item2Index] = temp;
+    }
+
+    // add item
+    add(item: Item)
+    {
+        const index = this._items.indexOf(undefined);
+        if (index === -1)
+            return;
+
+        this._items[index] = item;
+    }
+
+    // remove item
+    remove(item: Item)
+    {
+        const index = this._items.indexOf(item);
+        if (index === -1)
+            return;
+        
+        this._items[index] = undefined;
+    }
+
+    // check if inventory contains item
+    contains(item: Item): boolean
+    {
+        return this._items.indexOf(item) !== -1;
+    }
+
+    // check if inventory is full
+    isFull(): boolean
+    {
+        return this._items.indexOf(undefined) === -1;
+    }
+
     // add item to inventory
     public addItem(item: Item)
     {
