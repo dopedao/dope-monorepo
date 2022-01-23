63478ab75777cbcf3e456b8fa21cdb9ba21606f8
Larko <59736843+Larkooo@users.noreply.github.com>
2022-01-23 16:16:21 -0500

    chore: dispatching events, added mutex to containers & fixed struct structures

--- packages/api/game/types.go
+++ packages/api/game/types.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"sync"
 
+	"github.com/dopedao/dope-monorepo/packages/api/base"
 	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
 )
@@ -14,118 +16,255 @@ type BaseMessage struct {
 	Data  json.RawMessage `json:"data"`
 }
 
+type PlayerJoinClientData struct {
+	Id   string  `json:"id"`
+	Name string  `json:"name"`
+	X    float32 `json:"x"`
+	Y    float32 `json:"y"`
+}
+
 type PlayerJoinData struct {
-	name string
-	x    float32
-	y    float32
+	Name string  `json:"name"`
+	X    float32 `json:"x"`
+	Y    float32 `json:"y"`
 }
 
 type PlayerMoveData struct {
-	id string
-	x  float32
-	y  float32
+	Id string  `json:"id"`
+	X  float32 `json:"x"`
+	Y  float32 `json:"y"`
 }
 
 type IdData struct {
-	id string
+	Id string `json:"id"`
 }
 
 type ItemEntityCreateData struct {
-	item string
-	x    float32
-	y    float32
+	Item string  `json:"item"`
+	X    float32 `json:"x"`
+	Y    float32 `json:"y"`
+}
+
+type PlayersContainer struct {
+	data  []*Player
+	mutex sync.Mutex
+}
+
+type ItemEntitiesContainer struct {
+	data  []*ItemEntity
+	mutex sync.Mutex
 }
 
 type Game struct {
-	ctx          context.Context
-	connection   *websocket.Conn
-	players      []*Player
-	itemEntities []*ItemEntity
+	players      PlayersContainer
+	itemEntities ItemEntitiesContainer
+
+	// playerMessages chan *BaseMessage
 }
 
-func (g *Game) HandlePlayerJoin(data PlayerJoinData) {
-	fmt.Println("player join")
+func (g *Game) PlayerByUUID(ctx context.Context, uuid uuid.UUID) *Player {
+	g.players.mutex.Lock()
+	defer g.players.mutex.Unlock()
+
+	for _, player := range g.players.data {
+		if player.id == uuid {
+			return player
+		}
+	}
+	return nil
+}
+
+func (g *Game) PlayerByConn(ctx context.Context, conn *websocket.Conn) *Player {
+	g.players.mutex.Lock()
+	defer g.players.mutex.Unlock()
+
+	for _, player := range g.players.data {
+		if player.conn == conn {
+			return player
+		}
+	}
+	return nil
+}
+
+func (g *Game) DispatchPlayerJoin(ctx context.Context, player *Player) {
+	_, log := base.LogFor(ctx)
+
+	joinData, err := json.Marshal(PlayerJoinClientData{
+		Id:   player.id.String(),
+		Name: player.name,
+		X:    player.x,
+		Y:    player.y,
+	})
+	if err != nil {
+		log.Err(err).Msg("could not marshal join data")
+		return
+	}
+
+	fmt.Println(g.players.data)
+
+	// tell every other player that this player joined
+	for _, otherPlayer := range g.players.data {
+		if player.id == otherPlayer.id {
+			continue
+		}
+
+		// player join message
+		otherPlayer.conn.WriteJSON(BaseMessage{
+			Event: "player_join",
+			Data:  joinData,
+		})
+	}
+}
+
+func (g *Game) HandlePlayerJoin(ctx context.Context, conn *websocket.Conn, data PlayerJoinData) {
+	g.players.mutex.Lock()
+	defer g.players.mutex.Unlock()
+
+	_, log := base.LogFor(ctx)
 
 	player := &Player{
+		conn: conn,
+
 		id:   uuid.New(),
-		name: data.name,
-		x:    data.x,
-		y:    data.y,
+		name: data.Name,
+		x:    data.X,
+		y:    data.Y,
 	}
 
-	playersPatched := append(g.players, player)
-	fmt.Println("players patched: ", playersPatched)
-}
-
-func (g *Game) HandlePlayerLeave(data IdData) {
-	fmt.Println("player leave")
-
-	for i, player := range g.players {
-		uuid, err := uuid.FromBytes([]byte(data.id))
-		if err != nil {
-			fmt.Println("could not parse uuid: ", err)
-			continue
-		}
-
-		if player.id == uuid {
-			g.players = append(g.players[:i], g.players[i+1:]...)
-			break
-		}
+	g.players.data = append(g.players.data, player)
+	handShakeData, err := json.Marshal(IdData{Id: player.id.String()})
+	if err != nil {
+		log.Err(err).Msg("could not marshal handshake data")
+		// remove player
+		g.players.data = append(g.players.data[:len(g.players.data)-1], g.players.data[len(g.players.data)-1+1:]...)
+		return
 	}
-}
 
-func (g *Game) HandlePlayerMove(data PlayerMoveData) {
-	fmt.Println("player move")
-
-	for i, player := range g.players {
-		uuid, err := uuid.FromBytes([]byte(data.id))
-		if err != nil {
-			fmt.Println("could not parse uuid: ", err)
-			continue
-		}
-
-		if player.id == uuid {
-			g.players[i].x = data.x
-			g.players[i].y = data.y
-			break
-		}
-	}
-}
-
-func (g *Game) HandleItemEntityCreate(data ItemEntityCreateData) {
-	fmt.Println("item entity create")
-
-	itemEntitiesPatched := append(g.itemEntities, &ItemEntity{
-		id:   uuid.New(),
-		item: data.item,
-		x:    data.x,
-		y:    data.y,
+	// send back id to player
+	conn.WriteJSON(BaseMessage{
+		Event: "player_handshake",
+		Data:  handShakeData,
 	})
-	fmt.Println("item entities patched: ", itemEntitiesPatched)
+
+	g.DispatchPlayerJoin(ctx, player)
+
+	log.Info().Msgf("player joined: %s | %s", player.id, player.name)
 }
 
-func (g *Game) HandleItemEntityDestroy(data IdData) {
-	fmt.Println("item entity destroy")
+func (g *Game) DispatchPlayerLeave(ctx context.Context, player *Player) {
+	_, log := base.LogFor(ctx)
 
-	for i, itemEntity := range g.itemEntities {
-		uuid, err := uuid.FromBytes([]byte(data.id))
-		if err != nil {
-			fmt.Println("could not parse uuid: ", err)
+	leaveData, err := json.Marshal(IdData{Id: player.id.String()})
+	if err != nil {
+		log.Err(err).Msg("could not marshal leave data")
+		return
+	}
+
+	// tell every other player that this player left
+	for _, otherPlayer := range g.players.data {
+		if player.id == otherPlayer.id {
 			continue
 		}
 
-		if itemEntity.id == uuid {
-			g.itemEntities = append(g.itemEntities[:i], g.itemEntities[i+1:]...)
+		// player leave message
+		otherPlayer.conn.WriteJSON(BaseMessage{
+			Event: "player_leave",
+			Data:  leaveData,
+		})
+	}
+}
+
+func (g *Game) HandlePlayerLeave(ctx context.Context, conn *websocket.Conn, data IdData) {
+	g.players.mutex.Lock()
+	defer g.players.mutex.Unlock()
+
+	_, log := base.LogFor(ctx)
+
+	uuid, err := uuid.FromBytes([]byte(data.Id)[:32])
+	if err != nil {
+		log.Err(err).Msgf("could not parse uuid: %s", data.Id)
+		return
+	}
+
+	for i, player := range g.players.data {
+		if player.id == uuid {
+			g.players.data = append(g.players.data[:i], g.players.data[i+1:]...)
+			conn.Close()
+			g.DispatchPlayerLeave(ctx, player)
+			log.Info().Msgf("player left: %s | %s", player.id, player.name)
 			break
 		}
 	}
+}
+
+func (g *Game) HandlePlayerMove(ctx context.Context, data PlayerMoveData) {
+	g.players.mutex.Lock()
+	defer g.players.mutex.Unlock()
+
+	_, log := base.LogFor(ctx)
+
+	uuid, err := uuid.FromBytes([]byte(data.Id)[:32])
+	if err != nil {
+		log.Err(err).Msg("could not parse uuid: " + data.Id)
+		return
+	}
+
+	for i, player := range g.players.data {
+		if player.id == uuid {
+			g.players.data[i].x = data.X
+			g.players.data[i].y = data.Y
+			// TODO: send player move message to other players
+			break
+		}
+	}
+}
+
+func (g *Game) HandleItemEntityCreate(ctx context.Context, data ItemEntityCreateData) {
+	g.itemEntities.mutex.Lock()
+	defer g.itemEntities.mutex.Unlock()
+
+	_, log := base.LogFor(ctx)
+
+	g.itemEntities.data = append(g.itemEntities.data, &ItemEntity{
+		id:   uuid.New(),
+		item: data.Item,
+		x:    data.X,
+		y:    data.Y,
+	})
+	// TODO: send item entity create message to other players
+	log.Info().Msgf("item entity created: %s | %s", g.itemEntities.data[len(g.itemEntities.data)-1].id, data.Item)
+}
+
+func (g *Game) HandleItemEntityDestroy(ctx context.Context, data IdData) {
+	g.itemEntities.mutex.Lock()
+	defer g.itemEntities.mutex.Unlock()
+
+	_, log := base.LogFor(ctx)
+
+	uuid, err := uuid.FromBytes([]byte(data.Id)[:32])
+	if err != nil {
+		log.Err(err).Msgf("could not parse uuid: %s", data.Id)
+		return
+	}
+
+	for i, itemEntity := range g.itemEntities.data {
+		if itemEntity.id == uuid {
+			g.itemEntities.data = append(g.itemEntities.data[:i], g.itemEntities.data[i+1:]...)
+			break
+		}
+	}
+	// TODO: send item entity destroy message to other players
 }
 
 type Player struct {
+	conn *websocket.Conn
+
 	id   uuid.UUID
 	name string
 	x    float32
 	y    float32
+
+	// send chan *BaseMessage
 }
 
 type ItemEntity struct {
@@ -134,7 +273,3 @@ type ItemEntity struct {
 	x    float32
 	y    float32
 }
-
-// type Item struct {
-
-// }
