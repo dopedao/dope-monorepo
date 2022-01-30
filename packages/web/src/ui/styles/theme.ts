7cdafa7520978a8fbf58a43e5d6c1c9001a81e0e
subimage <seth@subimage.com>
2022-01-30 07:25:06 -0800

    Tweaked bgs

--- packages/web/src/ui/styles/theme.ts
+++ packages/web/src/ui/styles/theme.ts
@@ -4,17 +4,17 @@ const desktopImageCss = [
   // regular bgs uncomment after cny
   // "#000000 url('/images/desktop/TONY.gif') center center / contain repeat-y fixed",
   // "#d10913 url('/images/desktop/LAMBO.png') center center / cover repeat-y fixed",
-  // "#5f3084 url('/images/desktop/PAPER.png') center / 512px repeat",
-  // "#202221 url('/images/dope-wars-stacked-logo.png') center / 400px no-repeat fixed",
-  // "#202221 url('/images/dope-wars-smiley.png') center / 400px no-repeat fixed",
   // "#1d261c url('/images/desktop/limo-chopper.png') center / cover repeat-y",
+  // "#5f3084 url('/images/desktop/PAPER.png') center / 512px repeat",
+  "#202221 url('/images/dope-wars-stacked-logo.png') center / 400px no-repeat fixed",
+  "#202221 url('/images/dope-wars-smiley.png') center / 400px no-repeat fixed",
   "#000000 url('/images/desktop/cny/dragon_dance.jpg') center center / contain repeat fixed",
-  "#d10913 url('/images/desktop/cny/dragon_stick.jpg') center center / cover repeat fixed",
-  "#5f3084 url('/images/desktop/cny/dragon_walk.jpg') center / 72px repeat",
-  "#202221 url('/images/desktop/cny/dragon.jpg') center / cover no-repeat fixed",
+  "#202221 url('/images/desktop/cny/dragon_stick.jpg') center center / cover repeat fixed",
+  "#202221 url('/images/desktop/cny/dragon.jpg') center / 4096px no-repeat fixed",
   "#202221 url('/images/desktop/cny/kid_car.jpg') center / cover repeat fixed",
-  "#1d261c url('/images/desktop/cny/tiger_walk.jpg') center / cover repeat-y",
-  "#1d261c url('/images/desktop/cny/tiger.jpg') center / contain repeat-x",
+  "#202221 url('/images/desktop/cny/tiger.jpg') center / contain repeat-x",
+  "#202221 url('/images/desktop/cny/fireworks_2.png') center / 1024px repeat",
+  "#202221 url('/images/desktop/cny/fireworks_1.gif') center / 512px repeat",
 ];
 const randomImageIndex = Math.floor(Math.random() * desktopImageCss.length);
 const randomDesktopImageCss = desktopImageCss[randomImageIndex];
