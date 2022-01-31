b83fc9b9d9894ad179f5bc727d3f353721eb896c
broody <broody@gmail.com>
2022-01-31 06:40:42 -0800

    Updated navbar/tabs based on figma (#238)

--- packages/web/src/features/swap-meet/components/MarketFilterBar/styles.ts
+++ packages/web/src/features/swap-meet/components/MarketFilterBar/styles.ts
@@ -2,9 +2,8 @@ import { media } from 'ui/styles/mixins';
 import styled from '@emotion/styled';
 
 export const Container = styled.div`
-  padding: 8px;
-  background-color: #878783;
-  border-bottom: 1px solid #434345;
+  padding: 10px;
+  background-color: #202221;
   width: 100%;
   display: flex;
   flex-wrap: wrap;
@@ -12,31 +11,47 @@ export const Container = styled.div`
   gap: 8px;
 
   > * {
-    flex: 1 1 25%;
+    flex: 1 1 30%;
   }
   div:first-of-type {
     flex: 1 1 100%;
   }
-
   ${media.tablet`
-    height: 52px;
-    flex-flow: row nowrap;
-    gap: 16px;
-    > * {
-      flex: 1;
-    }
-    div:first-of-type {
-      flex: 1;
-    }
+    flex-flow: row;
   `}
-
-  input,
-  select,
   div.toggleButton {
-    border-collapse: collapse;
-    height: 32px;
-    border-radius: 0;
-    border: 1px solid #000;
+    min-width: 32px;
+    max-width: 32px;
+    cursor: pointer;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 2px solid #000;
+    border-radius: 5px;
     white-space: nowrap;
+    background-color: #434345;
+  },
+  div.toggleButton img {
+    height: 18px;
+    width: 18px;
+  },
+  .search {
+    color: #878783;
+    border: 2px solid #000;
+    cursor: text;
+    border-radius: 5px;
+    background-color: #141011;
+    box-shadow: inset 1px 1px 2px rgba(0, 0, 0, 0.25), inset -1px -1px 0px rgba(255, 255, 255, 0.25);
+  },
+  select {
+    color: #fff;
+    border: 2px solid #000;
+    cursor: pointer;
+    border-radius: 5px;
+    background-color: #434345;
+  },
+  select option {
+    background-color: #434345;
   }
+
 `;
