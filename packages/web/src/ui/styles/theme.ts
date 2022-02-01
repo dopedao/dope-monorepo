e3332ef443a1b0ced9efb05c9fb922358210b8ec
subimage <seth@subimage.com>
2022-01-31 16:03:09 -0800

    Background tweaks

--- packages/web/src/ui/styles/theme.ts
+++ packages/web/src/ui/styles/theme.ts
@@ -11,7 +11,7 @@ const desktopImageCss = [
   // "#202221 url('/images/dope-wars-stacked-logo.png') center / 400px no-repeat fixed",
   // "#202221 url('/images/dope-wars-smiley.png') center / 400px no-repeat fixed",
   "#000000 url('/images/desktop/cny/dragon_dance.jpg') center center / contain repeat fixed",
-  "#202221 url('/images/desktop/cny/dragon.jpg') center / 4096px no-repeat fixed",
+  "#202221 url('/images/desktop/cny/dragon.jpg') center / contain repeat-y fixed",
   "#202221 url('/images/desktop/cny/kid_car.jpg') center / cover repeat fixed",
   "#202221 url('/images/desktop/cny/tiger.jpg') center / contain repeat-x",
   "#202221 url('/images/desktop/cny/fireworks_2.png') center / 1024px repeat",
@@ -21,7 +21,7 @@ const desktopImageCss = [
   "#202221 url('/images/desktop/cny/lanterns_2.png') center / contain repeat",
   "#202221 url('/images/desktop/cny/lanterns_3.png') center / cover repeat",
   "#202221 url('/images/desktop/cny/overhead.png') center / cover repeat",
-  "#000 url('/images/desktop/cny/chinatown_illustration.jpg') center / contain no-repeat fixed",
+  "#000 url('/images/desktop/cny/chinatown_illustration.jpg') center / contain repeat-y fixed",
 ];
 const randomImageIndex = Math.floor(Math.random() * desktopImageCss.length);
 const randomDesktopImageCss = desktopImageCss[randomImageIndex];
