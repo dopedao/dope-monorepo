775d364a33ad2e77b7565b09ec8de5d746ee84cc
subimage <seth@subimage.com>
2022-01-31 19:15:09 -0800

    State for addresses that don't quality for airdrop

--- packages/web/src/components/panels/HongbaoPanel.tsx
+++ packages/web/src/components/panels/HongbaoPanel.tsx
@@ -18,23 +18,27 @@ const HongbaoPanel = () => {
   const { account } = useWeb3React();
 
   // if event.args.typ == 0
-  //      PAPER reward. event.args.value is the amount
+  //      PAPER reward. event.args.value is the numUnopenedEnvelopes
   // if event.args.typ == 1
   //      Item reward. event.args.value is the item id
   const [opens, setOpens] = useState<OpenedEvent[]>();
   const [claimed, setClaimed] = useState<boolean>();
 
-  const amount = useMemo(() => config.airdrop[account!].toString(), [account]);
+  const eligibleForAirdrop = Object.keys(config.airdrop).includes(account || '');
+  const numUnopenedEnvelopes = useMemo(() => config.airdrop[account!], [account]);
+
   useEffect(() => {
+    if (!eligibleForAirdrop) return;
     hongbao
       .claimed(
-        Buffer.from(solidityKeccak256(['address', 'uint256'], [account, amount]).slice(2), 'hex'),
+        Buffer.from(solidityKeccak256(['address', 'uint256'], [account, numUnopenedEnvelopes]).slice(2), 'hex'),
       )
       .then(setClaimed);
-  }, [hongbao, account, amount]);
+  }, [hongbao, account, numUnopenedEnvelopes]);
+
   const claim = useCallback(async () => {
-    const proof = getProof(account!, amount);
-    const tx = await hongbao.claim(amount, proof);
+    const proof = getProof(account!, numUnopenedEnvelopes.toString());
+    const tx = await hongbao.claim(numUnopenedEnvelopes, proof);
     const receipt = await tx.wait();
     setOpens(
       receipt.logs.reduce<OpenedEvent[]>((o, log, idx) => {
@@ -44,18 +48,46 @@ const HongbaoPanel = () => {
         return [...o, event];
       }, []),
     );
-  }, [hongbao, account, amount, setOpens]);
+  }, [hongbao, account, numUnopenedEnvelopes, setOpens]);
 
   return (
     <PanelContainer>
-      <PanelBody>
-        <Image src="/images/lunar_new_year_2022/hongbao-with-bg.png" alt="A surprise awaits you" />
-      </PanelBody>
-      <PanelFooter>
-        <Button variant="cny" onClick={claim} disabled={claimed}>
-          {claimed ? 'Already Claimed!' : `Open ${amount} Envelopes`}
-        </Button>
-      </PanelFooter>
+      { eligibleForAirdrop && <>
+        <PanelBody>
+          <Image 
+            src="/images/lunar_new_year_2022/hongbao-with-bg.png" 
+            alt="A surprise awaits you" 
+          />
+        </PanelBody>
+        <PanelFooter>
+          <Button variant="cny" onClick={claim} disabled={claimed}>
+            {claimed ? 'Already Claimed!' : `Open ${numUnopenedEnvelopes} Envelopes`}
+          </Button>
+        </PanelFooter>
+        </>
+      }
+      { !eligibleForAirdrop &&
+        <div css={css`
+          flex: 5;
+          height: 100%;
+          display:flex;
+          flex-direction: column;
+          align-items:center;
+          justify-content: center;
+          padding:32px;
+          background-color:#eee;
+          text-align: center;
+        `}>
+          <Image src="/images/icon/dope-smiley-sad.svg" width="72px" alt="Frowney Face" />
+          <br/>
+          <p>
+            The connected wallet is not eligible for this airdrop.
+          </p>
+          <p>
+            Purchase a rare accessory mask below!
+          </p>
+        </div>
+      }
     </PanelContainer>
   );
 };
