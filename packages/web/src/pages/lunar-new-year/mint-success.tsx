69e3a939dff0b223113e471da3a52a55c76d218f
subimage <seth@subimage.com>
2022-02-01 12:46:35 -0800

    Animation done

--- packages/web/src/pages/lunar-new-year/mint-success.tsx
+++ packages/web/src/pages/lunar-new-year/mint-success.tsx
@@ -3,20 +3,29 @@ import { Image } from '@chakra-ui/react';
 import AppWindowOptimism from 'components/AppWindowOptimism';
 import EventHeader from 'components/lunar_new_year/EventHeader';
 import styled from '@emotion/styled';
-import PanelContainer from 'components/PanelContainer';
-import PanelTitleHeader from 'components/PanelTitleHeader';
-import PanelBody from 'components/PanelBody';
 
 const FireworksShow = styled.div`
-  margin-top: -48px;
+  position: absolute;
+  z-index: 0;
+  top: 15%;
+  width: 100%;
+  height: 100%;
   display: flex;
   gap: 0;
   align-items: space-between;
   justify-content: center;
-  height: 100%;
   img {
     max-height: 70vh;
   }
+  @keyframes disappear {
+    from {
+      opacity: 1;
+    }
+    to {
+      opacity: 0;
+    }
+  }
+  animation: disappear 1s forwards 5.5s;
 `;
 const LogoContainer = styled.div`
   position: absolute;
@@ -37,29 +46,32 @@ const LogoContainer = styled.div`
       top: 0;
       opacity: 1;
     }
-    100% {
+    99% {
       top: 60%;
       opacity: 1;
     }
+    100% {
+      top: 60%;
+      opacity: 0;
+    }
   }
-  animation: scroll-in 5s linear 0s;
+  animation: scroll-in 5.2s linear forwards 0s;
 `;
 const MessageContainer = styled.div`
-  display: none;
   color: white;
   padding: 32px 64px;
   margin: 32px 25%;
   background-color: rgba(0,0,0,0.5);
-  visibility: visible;
-  // @keyframes appear {
-  //   0% {
-  //     visibility: hidden;
-  //   }
-  //   100% {
-  //     visibility: visible;
-  //   }
-  // }
-  // animation: appear 5s linear 5s;
+  opacity: 0;
+  @keyframes appear {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+  animation: appear 1s forwards 5.5s;
 `;
 
 const MintSuccess = () => {
