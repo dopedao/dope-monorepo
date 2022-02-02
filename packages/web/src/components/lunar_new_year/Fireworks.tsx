2a9a4829d68a6f6668dcb7cd60d6f4f3db27acc8
faces-of-eth <facesofeth@proton.me>
2022-02-01 20:35:44 -0800

    Odds and ends discovered after deployment (#250)
    * Fireworks
    
    * Responsive fixes
    
    * Responsive fixes for swap meet
    
    * Consistent padding
    
    * Fix spinner msg width

---
+++ packages/web/src/components/lunar_new_year/Fireworks.tsx
@@ -0,0 +1,43 @@
+import { getRandomNumber } from "utils/utils";
+import { Image } from "@chakra-ui/react";
+import { useState, useRef, useEffect } from "react";
+import styled from "@emotion/styled";
+import useInterval from "utils/use-interval";
+
+const Fireworks = () => {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const getRandomX = () => getRandomNumber(0, 100);
+  const getRandomY = () => getRandomNumber(0, 100);
+  const getRandomWidth = () => getRandomNumber(5, 33);
+
+  const [posX, setPosX] = useState(getRandomX());
+  const [posY, setPosY] = useState(getRandomY());
+  const [containerWidth, setContainerWidth] = useState(getRandomWidth());
+
+  const setRandomValues = () => {
+    setPosX(getRandomX());
+    setPosY(getRandomY());
+    setContainerWidth(getRandomWidth());    
+  }
+
+  useInterval(() => {
+    setRandomValues();
+  }, 1000);
+
+  const FireworkContainer = styled.div`
+    position:absolute;
+    display: inline-block;
+    z-index:2;
+    top: ${posY}%;
+    left: ${posX}%;
+    width: ${containerWidth}%;
+  `;
+  return(
+    <FireworkContainer ref={containerRef}>
+      <Image src="/images/lunar_new_year_2022/fireworks.gif" alt="fireworks" />
+    </FireworkContainer>
+  );
+}
+
+export default Fireworks;
