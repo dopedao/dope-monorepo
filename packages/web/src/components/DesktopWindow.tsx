4819fa5588f3e8062bd514582e477607bc749b38
subimage <seth@subimage.com>
2022-02-05 21:44:12 -0800

    Cookies are more simple than react context

--- packages/web/src/components/DesktopWindow.tsx
+++ packages/web/src/components/DesktopWindow.tsx
@@ -40,6 +40,7 @@ export type DesktopWindowProps = {
   loadingBalance?: boolean;
   children: ReactNode;
   onResize?: () => void;
+  onClose?: () => void;
   onMoved?: (position: any) => void;
   posX?: number;
   posY?: number;
@@ -108,6 +109,7 @@ const DesktopWindow = ({
   children,
   onResize,
   onMoved,
+  onClose,
   scrollable,
   posX = 0,
   posY = 0,
@@ -152,9 +154,7 @@ const DesktopWindow = ({
   const shouldBeDraggable = !isTouchDevice() && !isFullScreen && browserWidth > 768;
 
   useEffect(() => {
-    if (onResize) {
-      onResize();
-    }
+    if (onResize) onResize();
   }, [isFullScreen, onResize]);
 
   const handleStop = () => {
@@ -208,6 +208,7 @@ const DesktopWindow = ({
             loadingBalance={loading}
             windowRef={windowRef?.current}
             hideWalletAddress={hideWalletAddress}
+            onClose={onClose}
           >
             {titleChildren}
           </DesktopWindowTitleBar>
