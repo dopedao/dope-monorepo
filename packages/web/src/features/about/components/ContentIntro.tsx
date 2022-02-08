959724eda4376c520b860e29a233cf3d717b38ad
faces-of-eth <facesofeth@proton.me>
2022-02-08 04:17:30 -0800

    Copy

--- packages/web/src/features/about/components/ContentIntro.tsx
+++ packages/web/src/features/about/components/ContentIntro.tsx
@@ -1,13 +1,13 @@
 import MarkdownText from "components/MarkdownText";
 const ContentIntro = () => {
   const content = `
-Dope Wars is a fully decentralized, community driven, play-to-earn gaming metaverse project inspired by hip-hop culture. We believe in a fully decentralized world and our unique fully-customizable avatars in web3 stand for the values and principles we stand for with our carbon avatars.
+Dope Wars is a fully decentralized, community driven, play-to-earn gaming metaverse project inspired by hip-hop culture. Our unique fully-customizable avatars in web3 stand for the values and principles we stand for with our carbon avatars.
 
-We're bootstrapping a modern-day GTA-inspired metaverse and series of games on the Ethereum blockchain, [inspired by the classic Drugwars game](https://www.youtube.com/watch?v=3rg7rOfcdo8) that many of us played through the years – without VC funding.
+We're bootstrapping a modern-day GTA-inspired metaverse and series of games built by our decentralized game studio on the Ethereum blockchain, [inspired by the classic Drugwars game](https://www.youtube.com/watch?v=3rg7rOfcdo8) that many of us played through the years.
 
-We're creating an open environment that allows developers and contributors to produce their own limitless games for the Dope Wars NFT holders.
+Our open environment allows developers and contributors to produce their own limitless games for the Dope Wars NFT holders.
 
-Dope Wars is also an ongoing NFT art project and experiment in decentralized project governance using a [DAO](https://ethereum.org/en/dao/).
+Dope Wars is also an ongoing NFT art project and experiment in decentralized project finance and governance using a [DAO](https://ethereum.org/en/dao/). We have no VC funding. All of our treasury's revenue comes from royalties of NFT sales.
   `
   return <MarkdownText text={content} />
 };
