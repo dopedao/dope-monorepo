5b2391e3e4778508cdd5f328851857853061287b
subimage <seth@subimage.com>
2022-02-17 20:37:45 -0700

    Ensure we check for dope opened before init

--- packages/web/src/pages/hustlers/[id]/initiate.tsx
+++ packages/web/src/pages/hustlers/[id]/initiate.tsx
@@ -1,18 +1,58 @@
-import Steps from 'features/hustlers/modules/Steps';
-import HustlerProvider from 'features/hustlers/HustlerProvider';
+import { BigNumber } from 'ethers';
+import { Button } from '@chakra-ui/button';
+import { useEffect, useState } from 'react';
+import { useInitiator } from 'hooks/contracts';
+import { useRouter } from 'next/router';
 import AppWindowEthereum from 'components/AppWindowEthereum';
+import Dialog from 'components/Dialog';
+import HustlerProvider from 'features/hustlers/HustlerProvider';
+import Steps from 'features/hustlers/modules/Steps';
 
-const InitiatePage = () => (
-  <AppWindowEthereum
-    requiresWalletConnection={true}
-    scrollable={true}
-    title="Initiate Your Hustler"
-    padBody={false}
-  >
-    <HustlerProvider>
-      <Steps />
-    </HustlerProvider>
-  </AppWindowEthereum>
-);
+const InitiatePage = () => {
+  const init = useInitiator();
+  const router = useRouter();
+  const { id: dopeId } = router.query;
+  const [isOpened, setIsOpened] = useState();
+
+  useEffect(() => {
+    let isMounted = true;
+    if(!dopeId) return;
+    init.isOpened(BigNumber.from(dopeId)).then(value => {
+      if (isMounted) setIsOpened(value);
+    });
+    return () => {
+      isMounted = false;
+    };
+  }, [init, dopeId]);
+
+  return (
+    <AppWindowEthereum
+      requiresWalletConnection={true}
+      scrollable={true}
+      title="Initiate Your Hustler"
+      padBody={false}
+    >
+      {isOpened === true &&
+        <Dialog
+          title="Hustler already initiated"
+          icon="dope-smiley-sad"
+        >
+          <p>
+            DOPE #{dopeId} has already been Claimed.
+          </p>
+          <Button onClick={() => router.back() }>
+            Go Back
+          </Button>
+        </Dialog>
+      }
+      {isOpened === false &&
+        <HustlerProvider>
+          <Steps />
+        </HustlerProvider>
+      }
+
+    </AppWindowEthereum>
+  );
+}
 
 export default InitiatePage;
