794635baaed87dc73b4577aba5946e30019a271f
Jemil Ezzet <jemil.ezzet@gmail.com>
2022-02-19 14:53:01 -0800

    feat(ryo): add simple drugs table

--- packages/web/src/features/ryo/index.tsx
+++ packages/web/src/features/ryo/index.tsx
@@ -1,2 +1,23 @@
-const GameWindow = () => <div>game</div>;
+import DopeWarsExeNav from 'components/DopeWarsExeNav';
+import AppWindow from 'components/AppWindow';
+import Head from 'components/Head';
+
+import Container from "./components/Container";
+import Drugs from './components/Drugs';
+
+const GameWindow = () => {
+  return (
+    <AppWindow
+      background='#202221'
+      padBody={false}
+      scrollable={false}
+      height="90vh"
+      title="Roll your own"
+    >
+      {/* <Head title="ROLL YOUR OWN" /> */}
+      <Drugs />
+    </AppWindow>
+  )
+};
+
 export default GameWindow;
