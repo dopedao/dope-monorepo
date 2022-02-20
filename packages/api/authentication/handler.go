6767bf6386ad35a580aef992f8d9cee55a34a015
Larko <59736843+Larkooo@users.noreply.github.com>
2022-02-20 15:55:29 -0500

    chore: using siwe go library and handling basic authentication

--- packages/api/authentication/handler.go
+++ packages/api/authentication/handler.go
@@ -1,12 +1,15 @@
 package authentication
 
 import (
+	"encoding/hex"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"os"
 	"time"
 
 	"github.com/gorilla/sessions"
+	"github.com/jiulongw/siwe-go"
 )
 
 type LoginBody struct {
@@ -16,24 +19,26 @@ type LoginBody struct {
 
 var store = sessions.NewCookieStore([]byte(os.Getenv("SESSION_KEY")))
 
-// Generates a nonce for the session.
+// Generates a nonce for the session
 func NonceHandler() func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// Generate a 12 bytes (96 bits) randomized string
-		nonce := Bytes(22)
 		session, err := store.Get(r, "session")
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
+		nonce := String(12)
+
 		session.Values["nonce"] = nonce
 
+		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(200)
-		w.Write(nonce)
+		w.Write([]byte(nonce))
 	}
 }
 
+// Validates signed payload with nonce
 func LoginHandler() func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var body LoginBody
@@ -44,31 +49,55 @@ func LoginHandler() func(http.ResponseWriter, *http.Request) {
 			return
 		}
 
-		message, err := ParseMessage(body.Message)
+		siweMessage, err := siwe.MessageFromString(body.Message)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
+		signature, err := hex.DecodeString(body.Signature[2:])
+		if err != nil {
+			http.Error(w, fmt.Sprintf("invalid signature: %s", err.Error()), http.StatusBadRequest)
+			return
+		}
+
+		if err := siweMessage.Verify(signature); err != nil {
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
+
 		session, err := store.Get(r, "session")
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		if !message.Validate(body.Signature) || session.Values["nonce"] != message.Nonce {
-			http.Error(w, "invalid nonce or signature", http.StatusUnauthorized)
-			return
+		if siweMessage.ExpirationTime != nil {
+			session.Options.MaxAge = int(time.Until(*siweMessage.ExpirationTime).Seconds())
 		}
 
-		parsedTime, err := time.Parse(time.RFC3339, message.ExpirationTime)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-
-		session.Values["siwe"] = message
-		session.Options.MaxAge = int(time.Until(parsedTime).Seconds())
+		session.Values["siwe"] = body.Message
 		session.Save(r, w)
+
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
+	}
+}
+
+func AuthorizedHandler() func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		session, err := store.Get(r, "session")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		if session.Values["siwe"] == nil {
+			http.Error(w, "unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		w.WriteHeader(200)
+		w.Write([]byte("OK"))
 	}
 }
 
@@ -86,5 +115,6 @@ func LogoutHandler() func(http.ResponseWriter, *http.Request) {
 		}
 
 		session.Options.MaxAge = -1
+		session.Save(r, w)
 	}
 }
