6805f16342e00c3402578f9db95af5f9e0505e11
Tarrence van As <tarrence@vanas.family>
2022-02-20 21:43:14 -0500

    feat(ryo): hookup user registration

--- packages/web/src/features/ryo/components/Lobby.tsx
+++ packages/web/src/features/ryo/components/Lobby.tsx
@@ -1,9 +1,17 @@
+import { useMemo } from 'react';
+import { Button, Link } from '@chakra-ui/react';
 import { useStarknet, useStarknetInvoke } from '@starknet-react/core';
 import { useUserRegistryContract } from 'hooks/contracts/roll-your-own';
 
 const Lobby = () => {
   const { account } = useStarknet();
   const registry = useUserRegistryContract();
+
+  const isRegistered = useMemo(
+    () => registry?.call('get_user_info', { user_id: account! }),
+    [account, registry],
+  );
+
   const { invoke, data, loading, error } = useStarknetInvoke({
     contract: registry,
     method: 'register_user',
@@ -12,15 +20,21 @@ const Lobby = () => {
   return (
     <div>
       Lobby
-      <button
-        onClick={() =>
-          invoke({
-            args: { user_id: account, data: '84622096520155505419920978765481155' },
-          })
-        }
-      >
-        join
-      </button>
+      {isRegistered ? (
+        <Link href="/roll-your-own/1" passHref>
+          <Button>Play</Button>
+        </Link>
+      ) : (
+        <Button
+          onClick={() =>
+            invoke({
+              args: { user_id: account, data: '84622096520155505419920978765481155' },
+            })
+          }
+        >
+          Join
+        </Button>
+      )}
     </div>
   );
 };
