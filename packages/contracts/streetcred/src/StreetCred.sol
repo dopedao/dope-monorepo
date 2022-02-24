cc97a17bba61709daaaec02ee918ca588d665d94
Tarrence van As <tarrence@vanas.family>
2022-02-24 14:48:06 -0500

    feat(contracts): streetcred

--- packages/contracts/streetcred/src/StreetCred.sol
+++ packages/contracts/streetcred/src/StreetCred.sol
@@ -3,23 +3,35 @@ pragma solidity ^0.8.9;
 
 import { ERC20 } from 'openzeppelin-contracts/token/ERC20/ERC20.sol';
 import { ERC20Permit } from 'openzeppelin-contracts/token/ERC20/extensions/draft-ERC20Permit.sol';
-import { Ownable } from 'openzeppelin-contracts/access/Ownable.sol';
+import { AccessControl } from 'openzeppelin-contracts/access/AccessControl.sol';
 import { L2ERC20Votes } from 'rollcall/standards/L2ERC20Votes.sol';
 import { IL2VotingERC20 } from 'rollcall/standards/IL2VotingERC20.sol';
 
-contract StreetCred is ERC20, ERC20Permit, L2ERC20Votes, Ownable {
-    /**
-     * @param _name ERC20 name.
-     * @param _symbol ERC20 symbol.
-     */
-    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) ERC20Permit(_name) {}
+contract StreetCred is ERC20, ERC20Permit, L2ERC20Votes, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256('MINTER_ROLE');
+    bytes32 public constant BURNER_ROLE = keccak256('BURNER_ROLE');
+    bytes32 public constant TRANSFERRER_ROLE = keccak256('TRANSFERRER_ROLE');
 
-    function mint(address _to, uint256 _amount) public virtual onlyOwner {
+    constructor() ERC20('Streetcred', 'STREETCRED') ERC20Permit('Streetcred') {}
+
+    function mint(address _to, uint256 _amount) public virtual onlyRole(MINTER_ROLE) {
         _mint(_to, _amount);
     }
 
-    function burn(address _from, uint256 _amount) public virtual onlyOwner {
+    function burn(address _from, uint256 _amount) public virtual onlyRole(BURNER_ROLE) {
         _burn(_from, _amount);
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public override onlyRole(TRANSFERRER_ROLE) returns (bool) {
+        return super.transferFrom(from, to, amount);
+    }
+
+    function transfer(address to, uint256 amount) public override onlyRole(TRANSFERRER_ROLE) returns (bool) {
+        return super.transfer(to, amount);
     }
 
     function _afterTokenTransfer(
