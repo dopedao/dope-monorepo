5fecae6ae9f7b127e9af6f9baadd493f3d1bf20d
hiccupsoft <hiccup1991@hotmail.com>
2022-03-25 18:44:05 -0400

    use API

--- packages/web/src/pages/hustlers/[id]/flex.tsx
+++ packages/web/src/pages/hustlers/[id]/flex.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 // import { css } from '@emotion/react';
-// import { DopeLegendColors } from 'features/dope/components/DopeLegend';
+import { DopeLegendColors } from 'features/dope/components/DopeLegend';
 import { Grid, GridItem, Image, Box, Flex, Button } from '@chakra-ui/react';
 import { HustlerSex, HustlerCustomization } from 'utils/HustlerConfig';
 import { Item, useHustlerQuery } from 'generated/graphql';
@@ -89,7 +89,7 @@ const FlexPage = () => {
   }, [data, hustlerId]);
 
   const itemRles = useHustlerRles(data?.hustlers?.edges?.[0]?.node);
-
+  enum ItemType {Weapon, Vehicle, Drug, Clothes, Hand, Waist, Foot, Neck, Ring, Accessory}
   const items = useMemo<Item[]>(() => {
     const hustler = data?.hustlers?.edges?.[0]?.node;
     if (hustler) {
@@ -147,15 +147,15 @@ const FlexPage = () => {
               <Box>OG Calico</Box>
             </Box>
             <Box display="flex" flexDirection="column" borderRadius={'3'} overflow="hidden" border="1px solid black" mb="3">
-              <Image src="/images/hustler/vote_male.png" alt="png" />
+              <Image src={onChainImage} alt="png" />
               <Box color="#979999" width={'full'} textAlign="center" py="1" bgColor="#434345"  borderTop="1px solid black">Not For Sale</Box>
             </Box>
             <Box display={'flex'} justifyContent="space-between" width={'full'} mb="5">
               <Button ><Image src="/images/hustler/download.svg" alt="download" /></Button>
               <Button ><Image src="/images/hustler/camera.svg" alt="download" /></Button>
-              <Button ><Image src="/images/hustler/share.svg" alt="download" /></Button>
-              <Button ><Image src="/images/hustler/edit.svg" alt="download" /></Button>
-              <Button ><Image src="/images/hustler/setting.svg" alt="download" /></Button>
+              <Button onClick={()=>{router.push({pathname: `/hustlers/${router.query.id}/flex`})}}><Image src="/images/hustler/share.svg" alt="download" /></Button>
+              <Button onClick={()=>{router.push({pathname: `/hustlers/${router.query.id}/customize`})}}><Image src="/images/hustler/edit.svg" alt="download" /></Button>
+              <Button onClick={()=>{router.push({pathname: `/hustlers/${router.query.id}/gear`})}}><Image src="/images/hustler/setting.svg" alt="download" /></Button>
             </Box>
             <Box display={"flex"} width={'full'} mb="5">
               <Button width={"100%"}>Create Swap</Button>
@@ -176,19 +176,19 @@ const FlexPage = () => {
               <Box p="2" display={'flex'} borderBottom={"1px solid #cfd9d4"} >
                 <Box color="#919191" flex={isMobile ? 2 : 1}>NAME</Box>
                 <Box display='flex' alignItems="center" flex={5}>
-                  <Box>Hobbes</Box>
+                  <Box>{hustlerConfig.name}</Box>
                 </Box>
               </Box>
               <Box p="2" display={'flex'} borderBottom={"1px solid #cfd9d4"} >
                 <Box color="#919191" flex={isMobile ? 2 : 1}>ID</Box>
                 <Box display='flex' alignItems="center" flex={5}>
-                  <Box>#88 OG</Box>
+                  <Box>{hustlerConfig.dopeId}</Box>
                 </Box>
               </Box>
               <Box p="2" display={'flex'} borderBottom={"1px solid #cfd9d4"} >
                 <Box color="#919191" flex={isMobile ? 2 : 1}>TITLE</Box>
                 <Box display='flex' alignItems="center" flex={5}>
-                  <Box>Kid of the Game Feared</Box>
+                  <Box>{hustlerConfig.title}</Box>
                 </Box>
               </Box>
               <Box p="2" display={'flex'} borderBottom={"1px solid #cfd9d4"} >
@@ -216,71 +216,71 @@ const FlexPage = () => {
               <Box p="2" display={'flex'} borderBottom={"1px solid #cfd9d4"} >
                 <Box color="#919191" flex={isMobile ? 2 : 1}>WEAPON</Box>
                 <Box display='flex' alignItems="center" flex={5}>
-                  <Box borderRadius={999} border="1px solid black" width={"3"} height={"3"}/>
-                  <Box ml="2">AK 47</Box>
+                  <Box borderRadius={999} border="1px solid black" bgColor={DopeLegendColors[items[ItemType.Weapon].tier]} width={"3"} height={"3"}/>
+                  <Box ml="2">{items[ItemType.Weapon].name}</Box>
                 </Box>
               </Box>
               <Box p="2" display={'flex'} borderBottom={"1px solid #cfd9d4"} >
                 <Box color="#919191" flex={isMobile ? 2 : 1}>VEHICLE</Box>
                 <Box display='flex' alignItems="center" flex={5}>
-                  <Box borderRadius={999} border="1px solid black" width={"3"} height={"3"}/>
-                  <Box ml="2">ATV</Box>
+                  <Box borderRadius={999} border="1px solid black" bgColor={DopeLegendColors[items[ItemType.Vehicle].tier]} width={"3"} height={"3"}/>
+                  <Box ml="2">{items[ItemType.Vehicle].name}</Box>
                 </Box>
               </Box>
               <Box p="2" display={'flex'} borderBottom={"1px solid #cfd9d4"} >
                 <Box color="#919191" flex={isMobile ? 2 : 1}>DRUG</Box>
                 <Box display='flex' alignItems="center" flex={5}>
-                  <Box borderRadius={999} bgColor="#4780F7" border="1px solid black" width={"3"} height={"3"}/>
-                  <Box ml="2">LSD from Big Smoke</Box>
+                  <Box borderRadius={999} bgColor={DopeLegendColors[items[ItemType.Drug].tier]} border="1px solid black" width={"3"} height={"3"}/>
+                  <Box ml="2">{items[ItemType.Drug].name}</Box>
                 </Box>
               </Box>
               <Box p="2" display={'flex'} borderBottom={"1px solid #cfd9d4"} >
                 <Box color="#919191" flex={isMobile ? 2 : 1}>CLOTHES</Box>
                 <Box display='flex' alignItems="center" flex={5}>
-                  <Box borderRadius={999} bgColor="#4780F7" border="1px solid black" width={"3"} height={"3"}/>
-                  <Box ml="2">Black Hoodie from Murdertown</Box>
+                  <Box borderRadius={999} bgColor={DopeLegendColors[items[ItemType.Clothes].tier]} border="1px solid black" width={"3"} height={"3"}/>
+                  <Box ml="2">{items[ItemType.Clothes].name}</Box>
                 </Box>
               </Box>
               <Box p="2" display={'flex'} >
                 <Box color="#919191" flex={isMobile ? 2 : 1}>HANDS</Box>
                 <Box display='flex' alignItems="center" flex={5}>
-                  <Box borderRadius={999} border="1px solid black" width={"3"} height={"3"}/>
-                  <Box ml="2">Studed Leather Glove</Box>
+                  <Box borderRadius={999} bgColor={DopeLegendColors[items[ItemType.Hand].tier]} border="1px solid black" width={"3"} height={"3"}/>
+                  <Box ml="2">{items[ItemType.Hand].name}</Box>
                 </Box>
               </Box>
               <Box p="2" display={'flex'} >
                 <Box color="#919191" flex={isMobile ? 2 : 1}>SHOES</Box>
                 <Box display='flex' alignItems="center" flex={5}>
-                  <Box borderRadius={999} bgColor="#4780F7" border="1px solid black" width={"3"} height={"3"}/>
-                  <Box ml="2">Reebok Classics from Chicago</Box>
+                  <Box borderRadius={999} bgColor={DopeLegendColors[items[ItemType.Foot].tier]} border="1px solid black" width={"3"} height={"3"}/>
+                  <Box ml="2">{items[ItemType.Foot].name}</Box>
                 </Box>
               </Box>
               <Box p="2" display={'flex'} >
                 <Box color="#919191" flex={isMobile ? 2 : 1}>NECK</Box>
                 <Box display='flex' alignItems="center" flex={5}>
-                  <Box borderRadius={999} border="1px solid black" width={"3"} height={"3"}/>
-                  <Box ml="2">Gold Chain</Box>
+                  <Box borderRadius={999} bgColor={DopeLegendColors[items[ItemType.Neck].tier]} border="1px solid black" width={"3"} height={"3"}/>
+                  <Box ml="2">{items[ItemType.Neck].name}</Box>
                 </Box>
               </Box>
               <Box p="2" display={'flex'} >
                 <Box color="#919191" flex={isMobile ? 2 : 1}>RING</Box>
                 <Box display='flex' alignItems="center" flex={5}>
-                  <Box borderRadius={999} border="1px solid black" width={"3"} height={"3"}/>
-                  <Box ml="2">Silver Ring</Box>
+                  <Box borderRadius={999} bgColor={DopeLegendColors[items[ItemType.Ring].tier]} border="1px solid black" width={"3"} height={"3"}/>
+                  <Box ml="2">{items[ItemType.Ring].name}</Box>
                 </Box>
               </Box>
               <Box p="2" display={'flex'} >
                 <Box color="#919191" flex={isMobile ? 2 : 1}>WAIST</Box>
                 <Box display='flex' alignItems="center" flex={5}>
-                  <Box borderRadius={999} bgColor="#FEFC66" border="1px solid black" width={"3"} height={"3"}/>
-                  <Box ml="2">"John E. Dell Sin" Taser Holster from Oakland</Box>
+                  <Box borderRadius={999} bgColor={DopeLegendColors[items[ItemType.Waist].tier]} border="1px solid black" width={"3"} height={"3"}/>
+                  <Box ml="2">{items[ItemType.Waist].name}</Box>
                 </Box>
               </Box>
               <Box p="2" display={'flex'} >
                 <Box color="#919191" flex={isMobile ? 2 : 1}>ACCESSORY</Box>
                 <Box display='flex' alignItems="center" flex={5}>
-                  <Box borderRadius={999} bgColor="#EC6F38" border="1px solid black" width={"3"} height={"3"}/>
-                  <Box ml="2">“Celebratory” Dragon Mask from Chinatown</Box>
+                  <Box borderRadius={999} bgColor={DopeLegendColors[items[ItemType.Accessory].tier]} border="1px solid black" width={"3"} height={"3"}/>
+                  <Box ml="2">{items[ItemType.Accessory].name}</Box>
                 </Box>
               </Box>
             </Box>
