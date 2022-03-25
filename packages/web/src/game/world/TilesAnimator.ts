7b02fa08fab5c9fbe34eda55ba46e5f2a3ebc9f1
Larkooo <larko236@outlook.com>
2022-03-25 18:30:02 -0400

    fix: duration bug and rename

--- packages/web/src/game/world/TilesAnimator.ts
+++ packages/web/src/game/world/TilesAnimator.ts
@@ -1,6 +1,11 @@
+import GameScene from "game/scenes/Game";
 import { AnimationFrame, AsepriteAnimation } from "./AsepriteAnim";
 
-export default class AnimatedTiles {
+interface UpdateFrame extends AnimationFrame {
+    leftDuration: number;
+}
+
+export default class TilesAnimator {
     scene: Phaser.Scene;
     startingTiles: Phaser.Tilemaps.Tile[] = [];
     layer: Phaser.Tilemaps.TilemapLayer;
@@ -8,8 +13,11 @@ export default class AnimatedTiles {
     asepriteAnimation: AsepriteAnimation;
     currentFrame: number = 0;
 
+    updateCallback?: (time: number, delta: number) => void;
 
-    // animationSize: Phaser.Math.Vector2; // tile size
+    get started() {
+        return this.updateCallback !== undefined;
+    }
 
     constructor(scene: Phaser.Scene, startingTileIndex: number, layer: Phaser.Tilemaps.TilemapLayer, animationTileset: string) {
         this.scene = scene;
@@ -28,22 +36,30 @@ export default class AnimatedTiles {
     }
 
     start() {
-        const animationTileset = this.layer.tileset.find(t => t.name === this.asepriteName)!;
-        const frames = Object.values(this.asepriteAnimation.frames);
+        if (this.started) return;
 
-        this.scene.events.on(Phaser.Scenes.Events.PRE_UPDATE, (time: number, delta: number) => this.updateTiles(time, delta, frames, animationTileset));
+        const animationTileset = this.layer.tileset.find(t => t.name === this.asepriteName)!;
+        const frames = Object.values(this.asepriteAnimation.frames).map(f => {
+            (f as UpdateFrame).leftDuration = f.duration;
+            return f as UpdateFrame;
+        });
+
+        this.updateCallback = (time: number, delta: number) => this.updateTiles(time, delta, frames, animationTileset);
+        this.scene.events.on(Phaser.Scenes.Events.PRE_UPDATE, this.updateCallback);
     }
 
     stop() {
-        this.scene.events.off(Phaser.Scenes.Events.PRE_UPDATE, this.updateTiles);
+        if (!this.started) return;
+
+        this.scene.events.off(Phaser.Scenes.Events.PRE_UPDATE, this.updateCallback);
+        this.updateCallback = undefined;
     }
 
-    updateTiles(time: number, delta: number, frames: AnimationFrame[], animationTileset: Phaser.Tilemaps.Tileset) {
-        frames[this.currentFrame].duration -= delta;
-        if (frames[this.currentFrame].duration <= 0) {
-            this.currentFrame++;
-            if (this.currentFrame >= frames.length)
-                this.currentFrame = 0;
+    updateTiles(time: number, delta: number, frames: UpdateFrame[], animationTileset: Phaser.Tilemaps.Tileset) {
+        frames[this.currentFrame].leftDuration -= delta;
+        if (frames[this.currentFrame].leftDuration <= 0) {
+            frames[this.currentFrame].leftDuration = frames[this.currentFrame].duration;
+            this.currentFrame = (this.currentFrame + 1) % frames.length;
         }
 
         const frame = frames[this.currentFrame];
@@ -53,14 +69,15 @@ export default class AnimatedTiles {
         for (const startingTile of this.startingTiles) {
             for (let y = 0; y < frameNumberTiles.y; y++) {
                 for (let x = 0; x < frameNumberTiles.x; x++) {
-                    const tile = this.layer.getTileAt((startingTile.x) + x, startingTile.y + y);
+                    const tile = this.layer.getTileAt(startingTile.x + x, startingTile.y + y);
+                    if (!tile) continue;
+
                     const frameTilePos = new Phaser.Math.Vector2(
                         Math.floor(frame.frame.x / animationTileset.tileWidth) + x,
                         Math.floor(frame.frame.y / animationTileset.tileHeight) + y
                     );
 
-                    if (tile)
-                        tile.index = (animationTileset.firstgid) + (frameTilePos.x + (frameTilePos.y * animationTileset.columns));
+                    tile.index = animationTileset.firstgid + (frameTilePos.x + (frameTilePos.y * animationTileset.columns));
                 }
             }
         }
