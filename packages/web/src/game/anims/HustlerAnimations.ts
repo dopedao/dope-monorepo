cbbf86306701ab26a72b9837f9dc3c42f0b8a18d
Larkooo <larko236@outlook.com>
2022-03-27 23:14:42 -0400

    feat: implemented new spritesheet format & stand frames

--- packages/web/src/game/anims/HustlerAnimations.ts
+++ packages/web/src/game/anims/HustlerAnimations.ts
@@ -4,19 +4,25 @@ export const createHustlerAnimations = (
   scene: Phaser.Scene,
   key: string,
 ): void => {
+  const texture = scene.textures.get(key);
   const anims = scene.anims;
   const scale = 0.15;
 
   // TODO: move elsewhere
   // create icon frame
-  const frame = new Phaser.Textures.Frame(scene.textures.get(key), key + '_icon', 0, 0, 5, 32, 24);
-  (scene.textures.get(key).frames as any)[key + '_icon'] = frame;
+  texture.add(key + '_icon', 0, 240 + 10, 10, 35, 24);
+
+  // stand frames
+  texture.add(key + '_stand_right', 0, 0, 240, 60, 60);
+  texture.add(key + '_stand_back', 0, 0, 300, 60, 60);
+  texture.add(key + '_stand_left', 0, 0, 360, 60, 60);
+  texture.add(key + '_stand_front', 0, 0, 420, 60, 60);
 
   anims.create({
     key: key + '_right',
     frames: anims.generateFrameNumbers(key, {
-      start: 7,
-      end: 0, 
+      start: 0,
+      end: 7,
     }),
     frameRate: Hustler.DEFAULT_VELOCITY / scale,
     repeat: -1,
@@ -25,8 +31,8 @@ export const createHustlerAnimations = (
   anims.create({
     key: key + '_left',
     frames: anims.generateFrameNumbers(key, {
-      start: 39,
-      end: 32,
+      start: 32,
+      end: 39,
     }),
     frameRate: Hustler.DEFAULT_VELOCITY / scale,
     repeat: -1,
@@ -35,8 +41,8 @@ export const createHustlerAnimations = (
   anims.create({
     key: key + '_front',
     frames: anims.generateFrameNumbers(key, {
-      start: 55,
-      end: 48,
+      start: 48,
+      end: 55,
     }),
     frameRate: Hustler.DEFAULT_VELOCITY / scale,
     repeat: -1,
@@ -45,8 +51,8 @@ export const createHustlerAnimations = (
   anims.create({
     key: key + '_back',
     frames: anims.generateFrameNumbers(key, {
-      start: 23,
-      end: 16,
+      start: 16,
+      end: 23,
     }),
     frameRate: Hustler.DEFAULT_VELOCITY / scale,
     repeat: -1,
--- packages/web/src/game/anims/HustlerAnimator.ts
+++ packages/web/src/game/anims/HustlerAnimator.ts
@@ -12,11 +12,10 @@ export default class HustlerAnimator {
       return;
 
     if (this.hustler.moveDirection === Direction.None) {
+      const standFrame = this.hustler.texture.key + '_stand' + (this.hustler.lastDirection !== Direction.None ? this.hustler.lastDirection : Direction.East);
       // reset to the first frame of the anim
-      if (this.hustler.anims.currentAnim && !this.hustler.anims.currentFrame.isLast)
-      {
-        this.hustler.anims.setCurrentFrame(this.hustler.anims.currentAnim.getLastFrame());
-      }
+      if (this.hustler.frame.name !== standFrame)
+        this.hustler.setFrame(standFrame);
 
       this.hustler.stopAfterDelay(100);
       return;
