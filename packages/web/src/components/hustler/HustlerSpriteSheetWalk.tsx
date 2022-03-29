8175edf068af963e2114c07b4385ede2487e9ad1
subimage <seth@subimage.com>
2022-03-29 12:07:54 -0700

    Fix hustler sprite sheet walk

--- packages/web/src/components/hustler/HustlerSpriteSheetWalk.tsx
+++ packages/web/src/components/hustler/HustlerSpriteSheetWalk.tsx
@@ -1,8 +1,11 @@
 import styled from '@emotion/styled';
 
-const backgroundSize = 2400;
-const frameWidth = backgroundSize / 16;
-const frameHeight = backgroundSize / 8;
+const imageWidth = 960;
+const imageHeight = 480;
+const numFrames = 8;
+const sizeMultiplier = 5;
+const frameWidth = (imageWidth / 16) * sizeMultiplier;
+const frameHeight = (imageHeight / 8) * sizeMultiplier;
 
 // Standalone hustler animation test
 
@@ -10,17 +13,19 @@ const HustlerSpriteSheetWalk = styled.div<{ id?: string }>`
   width: ${frameWidth}px;
   height: ${frameHeight}px;
   background: url('https://api.dopewars.gg/hustlers/${({ id }) => id}/sprites/composite.png');
+  // 800% width so we see the first 8 frames
   background-size: auto 800%;
   image-rendering: pixelated;
+  // Slide in image from the left for 8 frames
   @keyframes walk {
-    0% {
-      background-position: ${-(backgroundSize / 2)}px 0;
-    }
-    100 % {
+    from {
       background-position: 0 0;
+    }
+    to {
+      background-position: ${-frameWidth * numFrames}px 0;
     }
   }
   animation-name: walk;
-  animation: walk 1s steps(8, start) infinite;
+  animation: walk 1s steps(${numFrames}) infinite;
 `;
 export default HustlerSpriteSheetWalk;
