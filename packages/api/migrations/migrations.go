2598c33542319b0088ee4f1671a140fae2764fb6
faces-of-eth <facesofeth@proton.me>
2022-04-19 23:12:48 -0700

    Embed migration sql file in GO to ensure filepath can't break on deploy

--- packages/api/migrations/migrations.go
+++ packages/api/migrations/migrations.go
@@ -2,22 +2,27 @@ package migrations
 
 import (
 	"context"
+	"embed"
 	"fmt"
-	"os"
 	"strings"
 
 	"entgo.io/ent/dialect/sql"
 	"entgo.io/ent/dialect/sql/schema"
-	"github.com/dopedao/dope-monorepo/packages/api/common"
+	"github.com/dopedao/dope-monorepo/packages/api/base"
 	"github.com/dopedao/dope-monorepo/packages/api/ent"
 )
 
+//go:embed 00_init_search_index.sql
+var f embed.FS
+
 func Migrate(ctx context.Context, drv *sql.Driver, dbClient *ent.Client) (string, error) {
+	_, log := base.LogFor(ctx)
+
+	log.Debug().Msg("Running ENT Auto-migration tool")
 	if err := dbClient.Schema.Create(ctx, schema.WithHooks(func(next schema.Creator) schema.Creator {
 		return schema.CreateFunc(func(ctx context.Context, tables ...*schema.Table) error {
 			var tables2 []*schema.Table
 			for _, t := range tables {
-				// Remove search_index since it is a materialized view
 				if t.Name != "search_index" {
 					tables2 = append(tables2, t)
 				}
@@ -27,8 +32,10 @@ func Migrate(ctx context.Context, drv *sql.Driver, dbClient *ent.Client) (string
 	})); err != nil {
 		return "", err
 	}
-	ts_migration, err := os.ReadFile("migrations/00_init_search_index.sql")
-	common.LogFatalOnErr(err, "Couldn't read migration file")
+
+	log.Debug().Msg("Loading SQL migrations for Materialized Views")
+
+	ts_migration, _ := f.ReadFile("00_init_search_index.sql")
 
 	if _, err := drv.DB().Exec(string(ts_migration)); err != nil {
 		if !strings.Contains(err.Error(), "already exists") {
