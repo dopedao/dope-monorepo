ddccca78715d35f7c4faa3792782ea97da8fb0f3
subimage <seth@subimage.com>
2022-06-22 15:07:17 -0700

    Stupid lint

--- packages/web/src/pages/invest.tsx
+++ packages/web/src/pages/invest.tsx
@@ -9,7 +9,7 @@ const Invest = () => {
       }}
       width="100%" 
       height="100%" 
-      src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FcJGby72SpqR6zwfgOwjDNE%2FPitch%3Fpage-id%3D0%253A1%26node-id%3D358%253A2316%26viewport%3D1997%252C-4996%252C0.21%26scaling%3Dscale-down-width%26starting-point-node-id%3D358%253A23166%26hide-ui%3D1" allowfullscreen>
+      src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FcJGby72SpqR6zwfgOwjDNE%2FPitch%3Fpage-id%3D0%253A1%26node-id%3D358%253A2316%26viewport%3D1997%252C-4996%252C0.21%26scaling%3Dscale-down-width%26starting-point-node-id%3D358%253A23166%26hide-ui%3D1" allowFullScreen>
       </iframe>
   );
 };
