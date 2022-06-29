0eb52c47c2f8573091cf93a0fec740e212381074
Oscar Guertler <oscarguertler@gmail.com>
2022-06-29 18:31:16 +0200

    make private

--- packages/api/web/verify/verify.go
+++ packages/api/web/verify/verify.go
@@ -13,7 +13,7 @@ import (
 )
 
 /* Structs */
-type DiscordOAuthObj struct {
+type discordOAuthObj struct {
 	Access_token  string `json:"access_token"`
 	Expires_in    int    `json:"expires_in"`
 	Refresh_token string `json:"refresh_token"`
@@ -21,19 +21,19 @@ type DiscordOAuthObj struct {
 	Token_type    string `json:"token_type"`
 }
 
-type DiscordApiUser struct {
+type discordApiUser struct {
 	Username      string `json:"username"`
 	Discriminator string `json:"discriminator"`
 	Id            string `json:"id"`
 	Email         string `json:"email"`
 }
 
-type DiscordGuild struct {
+type discordGuild struct {
 	Id string `json:"id"`
 }
 
 /* We send this to dopebot */
-type DopeBotData struct {
+type dopeBotData struct {
 	Id            string `json:"id"` /* Discord User Id */
 	WalletAddress string `json:"walletaddress"`
 	PaperCount    int    `json:"papercount"`
@@ -43,7 +43,7 @@ type DopeBotData struct {
 }
 
 /* We get this from dopewars.gg/verify  */
-type VerifyRequest struct {
+type verifyRequest struct {
 	DiscordToken  string `json:"discordtoken"`
 	WalletAddress string `json:"walletaddress"`
 	PaperCount    int    `json:"papercount"`
@@ -53,103 +53,20 @@ type VerifyRequest struct {
 }
 
 /* We send this back dopewars.gg/verify when we are done */
-type VerifyResponse struct {
+type verifyResponse struct {
 	Success bool   `json:"success"`
 	Message string `json:"message"`
 }
 
 /* global variables */
-const REDIRECT_URI = "http://localhost:3000/verify"
+const redirect_uri = "http://localhost:3000/verify"
 
-var DBOT_CLIENT_AUTH_TOKEN = os.Getenv("DBOT_CLIENT_OAUTH_TOKEN")
-var DBOT_CLIENT_ID = os.Getenv("DBOT_CLIENT_ID")
-var DBOT_GUILD_ID = os.Getenv("DBOT_GUILD_ID")
+var dbot_client_auth_token = os.Getenv("DBOT_CLIENT_OAUTH_TOKEN")
+var dbot_client_id = os.Getenv("DBOT_CLIENT_ID")
+var dbot_guild_id = os.Getenv("DBOT_GUILD_ID")
 
 /* function implementations */
-func sendErr(message string, w http.ResponseWriter) {
-	authRes := VerifyResponse{Message: message, Success: false}
-	encAuthRes, _ := json.Marshal(authRes)
-
-	w.WriteHeader(http.StatusInternalServerError)
-	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(encAuthRes))
-}
-
-func fetchDiscordAccesToken(discordToken string) (string, error) {
-	payload := url.Values{
-		"client_id":     {DBOT_CLIENT_ID},
-		"client_secret": {DBOT_CLIENT_AUTH_TOKEN},
-		"grant_type":    {"authorization_code"},
-		"code":          {discordToken},
-		"scope":         {"identify", "guilds", "email"},
-		"redirect_uri":  {REDIRECT_URI},
-	}
-
-	resp, err := http.Post("https://discord.com/api/oauth2/token", "application/x-www-form-urlencoded", strings.NewReader(payload.Encode()))
-	if err != nil {
-		return "", err
-	}
-
-	decoder := json.NewDecoder(resp.Body)
-	var discOauthObj DiscordOAuthObj
-	if err := decoder.Decode(&discOauthObj); err != nil {
-		return "", err
-	}
-
-	return discOauthObj.Access_token, nil
-}
-
-func fetchDiscordUser(accessToken string) (*DiscordApiUser, error) {
-	req, err := http.NewRequest("GET", "https://discord.com/api/users/@me", nil)
-	if err != nil {
-		return nil, err
-	}
-	req.Header.Add("Authorization", "Bearer "+accessToken)
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, err
-	}
-
-	var discApiUser DiscordApiUser
-	decoder := json.NewDecoder(resp.Body)
-	if err := decoder.Decode(&discApiUser); err != nil {
-		return nil, err
-	}
-
-	return &discApiUser, nil
-}
-
-func isUserInGuild(accessToken string) (bool, error) {
-	req, err := http.NewRequest("GET", "https://discord.com/api/users/@me/guilds", nil)
-	if err != nil {
-		return false, err
-	}
-
-	req.Header.Add("Authorization", "Bearer "+accessToken)
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return false, err
-	}
-
-	var discGuilds []DiscordGuild
-	decoder := json.NewDecoder(resp.Body)
-	if err := decoder.Decode(&discGuilds); err != nil {
-		return false, err
-	}
-
-	for _, guild := range discGuilds {
-		if guild.Id == DBOT_GUILD_ID {
-			return true, nil
-		}
-	}
-
-	return false, nil
-}
-
-func HandleVerifyRequest() func(http.ResponseWriter, *http.Request) {
+func HandleVerifyRequest(redisClient *redis.Client) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 		ctx, log := logger.LogFor(ctx)
@@ -157,7 +74,7 @@ func HandleVerifyRequest() func(http.ResponseWriter, *http.Request) {
 		decoder := json.NewDecoder(r.Body)
 		decoder.DisallowUnknownFields()
 
-		var verifyReq VerifyRequest
+		var verifyReq verifyRequest
 		if err := decoder.Decode(&verifyReq); err != nil {
 			log.Err(err)
 			sendErr("Something went wrong", w)
@@ -197,19 +114,13 @@ func HandleVerifyRequest() func(http.ResponseWriter, *http.Request) {
 			return
 		}
 
-		var dopeBotData DopeBotData
+		var dopeBotData dopeBotData
 		dopeBotData.Id = discUser.Id
 		dopeBotData.DopeCount = verifyReq.DopeCount
 		dopeBotData.HustlerCount = verifyReq.HustlerCount
 		dopeBotData.IsOg = verifyReq.IsOg
 		dopeBotData.PaperCount = verifyReq.PaperCount
 		dopeBotData.WalletAddress = verifyReq.WalletAddress
-
-		client := redis.NewClient(&redis.Options{
-			Addr:     "localhost:6379",
-			Password: "",
-			DB:       0,
-		})
 
 		encAuthReq, err := json.Marshal(dopeBotData)
 		if err != nil {
@@ -218,14 +129,14 @@ func HandleVerifyRequest() func(http.ResponseWriter, *http.Request) {
 			return
 		}
 
-		err = client.Publish(ctx, "discord", encAuthReq).Err()
+		err = redisClient.Publish(ctx, "discord", encAuthReq).Err()
 		if err != nil {
 			log.Err(err)
 			sendErr("Something went wrong", w)
 			return
 		}
 
-		authRes := VerifyResponse{Success: true}
+		authRes := verifyResponse{Success: true}
 		encAuthRes, err := json.Marshal(authRes)
 		if err != nil {
 			sendErr("Something went wrong", w)
@@ -236,4 +147,87 @@ func HandleVerifyRequest() func(http.ResponseWriter, *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.Write([]byte(encAuthRes))
 	}
+}
+
+func sendErr(message string, w http.ResponseWriter) {
+	authRes := verifyResponse{Message: message, Success: false}
+	encAuthRes, _ := json.Marshal(authRes)
+
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(encAuthRes))
+}
+
+func fetchDiscordAccesToken(discordToken string) (string, error) {
+	payload := url.Values{
+		"client_id":     {dbot_client_id},
+		"client_secret": {dbot_client_auth_token},
+		"grant_type":    {"authorization_code"},
+		"code":          {discordToken},
+		"scope":         {"identify", "guilds", "email"},
+		"redirect_uri":  {redirect_uri},
+	}
+
+	resp, err := http.Post("https://discord.com/api/oauth2/token", "application/x-www-form-urlencoded", strings.NewReader(payload.Encode()))
+	if err != nil {
+		return "", err
+	}
+
+	decoder := json.NewDecoder(resp.Body)
+	var discOauthObj discordOAuthObj
+	if err := decoder.Decode(&discOauthObj); err != nil {
+		return "", err
+	}
+
+	return discOauthObj.Access_token, nil
+}
+
+func fetchDiscordUser(accessToken string) (*discordApiUser, error) {
+	req, err := http.NewRequest("GET", "https://discord.com/api/users/@me", nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Add("Authorization", "Bearer "+accessToken)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	var discApiUser discordApiUser
+	decoder := json.NewDecoder(resp.Body)
+	if err := decoder.Decode(&discApiUser); err != nil {
+		return nil, err
+	}
+
+	return &discApiUser, nil
+}
+
+func isUserInGuild(accessToken string) (bool, error) {
+	req, err := http.NewRequest("GET", "https://discord.com/api/users/@me/guilds", nil)
+	if err != nil {
+		return false, err
+	}
+
+	req.Header.Add("Authorization", "Bearer "+accessToken)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return false, err
+	}
+
+	var discGuilds []discordGuild
+	decoder := json.NewDecoder(resp.Body)
+	if err := decoder.Decode(&discGuilds); err != nil {
+		return false, err
+	}
+
+	for _, guild := range discGuilds {
+		if guild.Id == dbot_guild_id {
+			return true, nil
+		}
+	}
+
+	return false, nil
 }
